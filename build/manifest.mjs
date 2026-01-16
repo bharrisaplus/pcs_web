ca723d6696607bdb0618e232525b93f36ff885fe
Barry Harris <bharrisaplus@gmail.com>
2026-01-16 00:21:51 -0500

    Move build target specific postcss config to build target folder

--- build/manifest.mjs
+++ build/manifest.mjs
@@ -5,6 +5,7 @@ import { default as NodePath } from 'node:path';
 const
 	_dir = import.meta.dirname,
 	 milieu = {
+	 	project_path: NodeProcess.env.PROJECT_DIR || NodePath.resolve(_dir, '../'),
 		behavior_path: NodeProcess.env.BEHAVIOR_DIR || NodePath.resolve(_dir, '../source/behavior')
 	};
 
