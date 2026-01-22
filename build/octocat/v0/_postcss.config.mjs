db2a375a528ba8243d88dab1f17931ccbfdee508
Barry Harris <bharrisaplus@gmail.com>
2026-01-22 13:35:31 -0500

    Ensure url is set for postcss sourcemap

--- build/octocat/v0/_postcss.config.mjs
+++ build/octocat/v0/_postcss.config.mjs
@@ -4,10 +4,24 @@ import { URL as NodeURL } from 'node:url';
 
 import { default as buildShared } from '../../manifest.mjs';
 
+const buildInfo = {
+	fhost: {
+		url: NodeProcess.env.DEMO_FILE_HOST || new NodeURL('../../../distribution/demo', import.meta.url),
+	},
+	lhost: {
+		url: NodeProcess.env.LHOST_HOST || new NodeURL('http://localhost:54321')
+	}
+};
 
 const buildConfig = {
 	cwd: buildShared.project_path,
-	map: NodeProcess.env.BUILD_AREA == 'prod' ? false : true,
+	map: NodeProcess.env.BUILD_AREA == 'prod' ? false : {
+		annotation: (postCSSOptTo) => {
+			let baseURL = NodeProcess.env.BUILD_AREA == 'lhost' ? buildInfo.lhost.url : buildInfo.fhost.url;
+
+			return `${baseURL.toString()}${NodePath.basename(postCSSOptTo)}.map`;
+		}
+	},
 	plugins: {
 		'postcss-combine-duplicated-selectors': {},
 		autoprefixer: {},
