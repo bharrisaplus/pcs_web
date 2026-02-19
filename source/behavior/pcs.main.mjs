44598340afbaa56e7a51c3f4240fc7be2ae81d2a
Barry Harris <bharrisaplus@gmail.com>
2026-02-19 00:14:29 -0500

    Show svg card after start event fires

--- source/behavior/pcs.main.mjs
+++ source/behavior/pcs.main.mjs
@@ -4,10 +4,16 @@ import miscHands from './hands/misc.hand.mjs';
 const
 	loadingIndicatorSelector = 'main > .loading-indicator-container',
 	loadingIndicatorSpinnySelector = '#pcs-card',
+	tableauSelector = '#tableau',
 	startEventName = 'pcsStart';
 
 document.addEventListener('DOMContentLoaded', () => {
 	window.addEventListener(startEventName, () => {
+		const $tableau = document.querySelector(tableauSelector);
+
+		$tableau.setAttribute('style', '');
+		$tableau.classList.remove('hide-before-load');
+
 		console.log("pcs started");
 	});
 
