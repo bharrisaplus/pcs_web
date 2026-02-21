fdbb01408cfa7bcb2c23060eed09bc7a2b45dd14
Barry Harris <bharrisaplus@gmail.com>
2026-02-21 01:27:26 -0500

    Add module to manage tableau behavior

---
+++ source/behavior/parts/tableau.part.mjs
@@ -0,0 +1,16 @@
+/**
+ * Component for cards
+ *
+ *    <tableau>
+ *      <card />
+ *      .
+ *      . <- (1-52)
+ *      .
+ *    </tableau>
+ *
+ * @typedef {Object} Tableau
+ * @property {[number]} currentOrder - ids of the cards in position
+ */
+
+
+export const debugName = "pcs:part:tableau";
