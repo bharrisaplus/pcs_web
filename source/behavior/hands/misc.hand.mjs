930ac47eb089ad35453e9f49d259391028688d23
Barry Harris <bharrisaplus@gmail.com>
2026-02-22 15:44:17 -0500

    Split asset loading into separate method and save console log check value

--- source/behavior/hands/misc.hand.mjs
+++ source/behavior/hands/misc.hand.mjs
@@ -8,46 +8,95 @@
  */
 const makeMiscHand = () => {
   let cycleCount = 0;
-  const currentLoc = window.location.href;
+  const console_free = (
+    window.location.href.startsWith('http://localhost:') ||
+    window.location.href.startsWith('https://localhost:') ||
+    window.location.href.startsWith('file:') // || whatever you like
+  );
+
+
+  /**
+   * Grab svg assets and inline them.
+   *   <html>
+   *     <head>
+   *       <prefetchLink /> <- element with an id that has an href to fetch
+   *     </head>
+   *     <body>
+   *       <footer>
+   *         <dump> <- element for appending to
+   *           <someAsset /> <- element may already be there
+   *         </dump>
+   *       </footer>
+   *     </body>
+   *   </html>
+   *
+   * 
+   * @param  {Map.<string, string>} assetMap The ids for elements to fetch and check for - {@link CSSStyleRule.selectorText}
+   * @param  {string} assetDump - {@link CSSStyleRule.selectorText}
+   */
+  const load_assets = (assetMap, assetDump) => {
+    const assetParser = new DOMParser();
+
+    assetMap.forEach(async (assetCheck, assetGrab) => {
+      if (document.querySelectorAll(assetCheck).length < 1) {
+        let assetUrl = document.querySelector(assetGrab)?.getAttribute('href');
+
+        if (assetUrl) {
+          assetResponse = await fetch(assetUrl),
+          assetInnards = await assetResponse.text();
+
+          if (assetInnards.length > 0) {
+            if (console_free) {
+              console.log(`Loading asset ${assetUrl}`);
+              console.log(`Placing within ${assetDump}`);
+            }
+
+            document.querySelector(assetMap.get("dump"))?.appendChild(
+              assetParser.parseFromString(assetInnards, 'image/svg+xml')
+            )
+          } else if (console_free) {
+            console.warn(`Failed loading asset ${assetUrl}`);
+          }
+        }
+      } else if (console_free) {
+        console.log(`Found ${assetCheck} asset inlined already`);
+      }
+    });
+  };
 
 
   /**
    * Looking at some container with animated child for a cue. Once the child is done with
-   * 	it's animation the container will be removed from view then the event will trigger.
-   * 	Intended as a oneoff at pageload so the indicator will be removed from the DOM as well.
+   *  it's animation the container will be removed from view then the event will trigger.
+   *  Intended as a oneoff at pageload so the indicator will be removed from the DOM as well.
    *
    *    <indicator> <- this will transition out of view
    *      <tick/> <- this is animating and we'll let it run a bit
    *    </indicator>
    *
-   * @param {Map.<string, string>} assetMap The element ids for assets - {@link CSSStyleRule.selectorText}
    * @param  {string} indicatorSelector For container element of tick - {@link CSSStyleRule.selectorText}
    * @param  {string} tickSelector The animating element relative to the container - {@link CSSStyleRule.selectorText}
    * @param  {string} evtName The event to fire when done - {@link CustomEvent.type}
    */
-  const finish_loading_then = (assetMap, indicatorSelector, tickSelector, evtName) => {
-    const $indicator = document.querySelector(indicatorSelector);
+  const watch_for_indicator_tick = (indicatorSelector, tickSelector, evtName) => {
+    const
+      $indicator = document.querySelector(indicatorSelector),
+      $tick = $indicator?.querySelector(tickSelector);
 
-    assetMap.forEach((assetCheck, assetGrab) => {
-      if (assetGrab !== 'dump') {
-        if (document.querySelectorAll(assetCheck).length < 1) {
-          console.log(`Loading asset ${document.querySelector(assetGrab).getAttribute('href')}`);
-          console.log(`Placing within ${assetMap.get("dump")}`);
-        }
-      }
-    });
+    if (!$indicator || !$tick) window.dispatchEvent(new CustomEvent(evtName));
 
     // Once loading is done, disconnect loading indicator from DOM
-    $indicator.addEventListener('transitionend', (transEvt) => {
+    $indicator?.addEventListener('transitionend', (transEvt) => {
       if (transEvt.propertyName == 'opacity') {
-        console.log("Loaded, removing indicator");
+        if (console_free) console.log("Loaded, removing indicator");
+
         $indicator.remove();
         window.dispatchEvent(new CustomEvent(evtName));
       }
     }, { once: true });
 
     // Let the loading animation show off a bit before starting
-    $indicator.querySelector(tickSelector).addEventListener('animationiteration', () => {
+    $indicator.querySelector(tickSelector)?.addEventListener('animationiteration', () => {
         cycleCount++;
 
         if (cycleCount >= 3) {
@@ -59,28 +108,26 @@ const makeMiscHand = () => {
 
   /**
    * Putting things in place for the app to begin
-   * @param  {string} startSelector  Element to reveal - {@link CSSStyleRule.selectorText}
-   * @param  {string} startEventName Event to wait for - {@link CustomEvent.type}
+   * @param  {string} centerpieceSelector Element to reveal - {@link CSSStyleRule.selectorText}
+   * @param  {string} revealEventName Event to wait for - {@link CustomEvent.type}
    */
-  const start_setup = (startSelector, startEventName) => {
-    window.addEventListener(startEventName, () => {
-      const $start = document.querySelector(startSelector);
+  const setup_reveal = (centerpieceSelector, revealEventName) => {
+    window.addEventListener(revealEventName, () => {
+      const $start = document.querySelector(centerpieceSelector);
 
       $start.setAttribute('style', '');
       $start.classList.remove('hide-before-load');
 
-      console.log("pcs started");
+      console.info("pcs started");
     });
   };
 
 
   return Object.freeze({
-    startAfter: finish_loading_then,
-    afterStart: start_setup,
-    // Computed-s
-    get yapFriendly () {
-      return currentLoc.startsWith('http://localhost:') || currentLoc.startsWith('file://')
-    }
+    warmUp: load_assets,
+    startAfter: watch_for_indicator_tick,
+    afterStart: setup_reveal,
+    yapFriendly: console_free
   });
 };
 
--- source/behavior/index.main.mjs
+++ source/behavior/index.main.mjs
@@ -2,25 +2,23 @@ import appCustodian from './hands/misc.hand.mjs';
 import buildAppScreen from './lattices/pcs.lattice.mjs';
 
 const
-	bootOverlay = '#pageload-curtain',
-	bootOverlaySpinner = '.loading-spinny',
-	cardView = '#tableau',
-	cardOverlay = '#turntable',
-	kickOffEventName = 'pcsStart',
-  /** @type Map.<string, string> */
-  preloadThings = new Map([
-    ["dump", ".inline-svg-assets-here"],
-    ["#card-sot", "#card-sheet"]
-  ]);
+  bootOverlay = '#pageload-curtain',
+  bootOverlaySpinner = '.loading-spinny',
+  cardView = '#tableau',
+  cardOverlay = '#turntable',
+  kickOffEventName = 'pcsStart',
+  preloadDest = ".inline-svg-assets-here",
+  preloadThings = new Map([["#card-sot", "#card-sheet"]]);
 
 document.addEventListener('DOMContentLoaded', () => {
-	const appScreen = buildAppScreen(cardOverlay);
+  const appScreen = buildAppScreen(cardOverlay);
 
-	if (appCustodian.yapFriendly) {
-		console.log('Dev-ish environment');
-		console.debug(appScreen);
-	}
+  if (appCustodian.yapFriendly) {
+    console.debug('console friendly environment');
+    console.debug(appScreen);
+  }
 
-	appCustodian.afterStart(cardView, kickOffEventName);
-	appCustodian.startAfter(preloadThings, bootOverlay, bootOverlaySpinner, kickOffEventName);
+  appCustodian.warmUp(preloadThings, preloadDest);
+  appCustodian.afterStart(cardView, kickOffEventName);
+  appCustodian.startAfter(bootOverlay, bootOverlaySpinner, kickOffEventName);
 });
