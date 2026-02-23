34769ec38ae4066044cb1d41c873b28873b1ef8d
Barry Harris <bharrisaplus@gmail.com>
2026-02-22 20:11:42 -0500

    Update docs and names for landing lattice

--- source/behavior/lattices/landing.lattice.mjs
+++ source/behavior/lattices/landing.lattice.mjs
@@ -1,41 +1,42 @@
-import getTurntable from '../parts/turntable.part.mjs';
-import getDealer from '../hands/dealer.hand.mjs';
 
 /**
- * @import {PCSLattice} from "../_meta/_typedefs.mjs"
+ * @import {PCSLattice, Tableau, Turntable, DeckBank} from "../_meta/_typedefs.mjs"
  */
+
+import getTurntable from '../parts/turntable.part.mjs';
+import getDealer from '../hands/dealer.hand.mjs';
 
 
 const latticeDealer = getDealer();
 
 /**
- * @param  {string} tableauID The primary element - {@link CSSStyleRule.selectorText}
- * @param  {string} turntableID The popover element - {@link CSSStyleRule.selectorText}
+ * @param  {string} tableauID The dingus element - {@link Tableau}
+ * @param  {string} turntableID The hud element - {@link Turntable}
  * @param  {DeckBank} itemVault The card state
  *
  * @return {PCSLattice}
  */
-const makePCSLattice = (tableauID, turntableID, itemVault) => {
+const scaffoldLattice = (tableauID, turntableID, itemVault) => {
   const
     $dingus = document.querySelector(tableauID),
-    $hud = getTurntable(turntableID);
+    hud = getTurntable(turntableID);
 
   if ($dingus) {
-    $dingus.querySelectorAll(`li.playing-card`)[0]?.addEventListener('click', $hud.loadTurntable);
-    $dingus.querySelectorAll(`li.playing-card`)[51]?.addEventListener('click', $hud.loadTurntable);
+    $dingus.querySelectorAll(`li.playing-card`)[0]?.addEventListener('click', hud.loadTurntable);
+    $dingus.querySelectorAll(`li.playing-card`)[51]?.addEventListener('click', hud.loadTurntable);
 
-    console.debug(latticeDealer.posRelLabels(0, itemVault))
-    console.debug(latticeDealer.posRelLabels(1, itemVault))
+    console.debug(latticeDealer.posRelLabels(0, itemVault));
+    console.debug(latticeDealer.posRelLabels(1, itemVault));
 
   } else {
     console.error("Could not find the tableau");
   }
 
   return Object.freeze({
-    pcsHUD: $hud
+    landingHUD: hud
   });
 };
 
 
-export default makePCSLattice;
+export default scaffoldLattice;
 export const debugName = "pcs:lattice:pcs";
