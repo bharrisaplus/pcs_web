b62bc903b180780119c75b4412a263000b6b403d
Barry Harris <bharrisaplus@gmail.com>
2026-03-13 22:52:34 -0400

    Add background color to store

--- source/behavior/banks/deck.bank.mjs
+++ source/behavior/banks/deck.bank.mjs
@@ -12,7 +12,9 @@ const default_order = Uint8Array.from({length: _g.c_Max}, (_, card_idx) => card_
 const makeDeckBank = () => {
   let
     topical_order = Uint8Array.from(default_order),
-    choosenCard;
+    choosen_card,
+    back_splash = 1;
+
 
   const new_deck_order = () => {
     topical_order = Uint8Array.from(default_order);
@@ -25,9 +27,17 @@ const makeDeckBank = () => {
     }
   };
 
+
+  const swap_back_splash_for = (newPaintChoice) => {
+    if (newPaintChoice > -1 && newPaintChoice < _g.dyes.length) {
+      back_splash = newPaintChoice;
+    }
+  }
+
+
   const choose_new_card = (cardID) => {
     if (topical_order.indexOf(cardID) > -1 && default_order.indexOf(cardID) > -1) {
-      choosenCard = cardID;
+      choosen_card = cardID;
     }
   };
 
@@ -35,10 +45,15 @@ const makeDeckBank = () => {
   return Object.freeze({
     updateCards: replace_topical_order_with,
     updateChoice: choose_new_card,
+    updateBackDrop: swap_back_splash_for,
     resetCards: new_deck_order,
     // Computed-s
     get choice () {
-      return [topical_order.indexOf(choosenCard), default_order.indexOf(choosenCard)];
+      return [topical_order.indexOf(choosen_card), default_order.indexOf(choosen_card)];
+    },
+
+    get backDrop () {
+      return back_splash;
     },
 
     get cards () {
