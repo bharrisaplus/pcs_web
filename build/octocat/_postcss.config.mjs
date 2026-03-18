d49e91111bb3dfbbce2306c86fe8d6e20b10e766
Barry Harris <bharrisaplus@gmail.com>
2026-03-18 15:02:42 -0400

    Remove filehost and localhost from octocat tasks and configs

--- build/octocat/_postcss.config.mjs
+++ build/octocat/_postcss.config.mjs
@@ -1,16 +1,12 @@
-import { default as NodeProcess } from 'node:process';
-import { default as NodePath } from 'node:path';
-import { URL as NodeURL } from 'node:url';
-
 import { default as buildShared } from '../manifest.mjs';
 
 const buildConfig = {
 	cwd: buildShared.project_path,
-	map: NodeProcess.env.BUILD_AREA == 'prod' ? false : true,
+	map: false,
 	plugins: {
 		'postcss-combine-duplicated-selectors': {},
 		autoprefixer: {},
-		cssnano: NodeProcess.env.BUILD_AREA == 'prod' ? { preset: 'default' } : false
+		cssnano: { preset: 'default' }
 	}
 };
 
--- build/octocat/_rollup.config.mjs
+++ build/octocat/_rollup.config.mjs
@@ -1,6 +1,5 @@
 import { default as NodeProcess } from 'node:process';
 import { default as NodePath } from 'node:path';
-import { URL as NodeURL } from 'node:url';
 
 import { default as RollupTerser } from '@rollup/plugin-terser';
 
@@ -12,7 +11,8 @@ const inputFilePath = NodePath.resolve(buildShared.behavior_path, 'index.main.mj
 let config;
 
 switch (NodeProcess.env.BUILD_AREA) {
-	case 'prod': {
+	case 'prod':
+	default: {
 		config = {
 		  input: inputFilePath,
 		  output: [{
@@ -20,35 +20,6 @@ switch (NodeProcess.env.BUILD_AREA) {
 				format: 'es',
 				name: 'PCS',
 				plugins: [ RollupTerser() ]
-			}]
-		}; break;
-	}
-	case 'lhost': {
-		config = {
-		  input: inputFilePath,
-		  output: [{
-				file: NodePath.resolve(buildShared.dev_path, buildShared.es_main),
-				format: 'es',
-				name: 'PCS',
-				plugins: [],
-				sourcemap: true,
-				sourcemapExcludeSources: true,
-				sourcemapBaseUrl: buildShared.localhost_url.toString()
-			}]
-		}; break;
-	}
-	case 'fhost':
-	default: {
-		config = {
-			input: inputFilePath,
-			output: [{
-				file: NodePath.resolve(buildShared.dev_path, buildShared.es_main),
-				format: 'es',
-				name: 'PCS',
-				plugins: [],
-				sourcemap: true,
-				sourcemapExcludeSources: true,
-				sourcemapBaseUrl: buildShared.filehost_url.toString()
 			}]
 		};
 	}
