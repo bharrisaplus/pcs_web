16e74d1d08b1fdfff6080c3a2c0a6f60f30b4e54
Barry Harris <bharrisaplus@gmail.com>
2026-03-18 21:11:44 -0400

    Add module to manage notification behavior

--- source/behavior/hands/scribe.hand.mjs
+++ source/behavior/hands/scribe.hand.mjs
@@ -7,11 +7,19 @@
  * @return {Readonly<Hand.Scribe>} a logger - {@link Hand.Scribe}
  */
 const makeScribeHand = () => {
-	const console_free = (
-	  window.location?.href?.startsWith('http://localhost:') ||
-	  window.location?.href?.startsWith('https://localhost:') ||
-	  window.location?.href?.startsWith('file:') // || whatever you like
-	);
+	/** @type {NotificationToast[]} */
+	let toasts = [];
+	const
+		console_free = (
+		  window.location?.href?.startsWith('http://localhost:') ||
+		  window.location?.href?.startsWith('https://localhost:') ||
+		  window.location?.href?.startsWith('file:') // || whatever you like
+		),
+
+		$toaster = document.querySelector('#notifications'),
+		/** @type {HTMLTemplateElement} */
+		$toastRef = document.querySelector('template#toast');
+
 
 	const log_dev = (devMsg, devThingy) => {
 		if (console_free) {
@@ -33,10 +41,67 @@ const makeScribeHand = () => {
 		}
 	};
 
+	const _make_toast = (toastMsg) => {
+		/** @type {NotificationToast} */
+		let maybeNoTo;
+		const
+			aToastAbort = new AbortController(),
+			/** @type {DocumentFragment} [description] */
+			$toastContainer = document.importNode($toastRef.content, true),
+			/** @type {HTMLElement} [description] */
+			$aToast = $toastContainer.querySelector('.toast');
+
+		if (toasts.some((_toastItm) => { _toastItm.$elm.textContent === toastMsg })) { return; }
+
+		$aToast.querySelector('span.toast-msg').textContent = toastMsg;
+		$aToast.querySelector('button.toast-close').addEventListener('click', (_clickEvt) => {
+			let
+				cutIdx,
+				cutToast;
+
+			if (_clickEvt.target.parentElement !== $aToast) { return; }
+
+			cutIdx = toasts.map((_toastItm) => _toastItm.$elm ).indexOf($aToast);
+
+			if (toasts.length === 1) {
+				$toaster.hidePopover();
+			}
+
+			if (cutIdx === -1) {
+				$aToast.remove();
+				return;
+			}
+
+			cutToast = toasts[cutIdx];
+			toasts = toasts.toSpliced(cutIdx, 1);
+
+			cutToast.cancel.abort();
+			cutToast.$elm.remove();
+		}, {signal: aToastAbort.signal});
+
+		$toaster.appendChild($aToast);
+		$toaster.showPopover();
+
+		if (toasts.length >= 3) {
+			const _gcToast = toasts.unshift();
+
+			_gcToast.cancel.abort();
+			_gcToast.$elm.remove();
+		}
+
+		maybeNoTo = {$elm: $aToast, cancel: aToastAbort}
+
+		toasts.push(maybeNoTo);
+	};
+
 
 	const log_notification = (notificationMsg) => {
 		if (console_free) {
 			if (notificationMsg) { console.info(notificationMsg); }
+		}
+
+		if ($toastRef && $toaster?.matches(`[popover="manual"]`)) {
+			_make_toast(notificationMsg);
 		}
 	};
 
