c542694d276de4c1f4c51baa21d78b514f10f8d9
Barry Harris <bharrisaplus@gmail.com>
2026-03-20 17:56:20 -0400

    Create example module to illustrate test example

---
+++ source/behavior/trythis.example.mjs
@@ -0,0 +1,44 @@
+/** Only used for test purposes. See test/specs/trythis_example.spec.mjs */
+
+/**
+ * @import {CSSelector} from './_meta/_typedefs.mjs';
+ */
+
+import { default as tryLogger } from './hands/scribe.hand.mjs';
+
+
+const someFunc = () => {
+	console.log("Thanks for trying");
+};
+
+
+const otherFunc = (aSelector) => {
+	document.querySelector(aSelector).addEventListener('click', () => {
+		console.log("Called event");
+	});
+};
+
+
+const anotherFunc = (elSelector) => {
+	document.querySelector(elSelector).addEventListener('click', () => {
+		tryLogger.devlog("Will dev log");
+	});
+}
+
+
+/**
+ * @typedef {Object} ExampleModule
+ * @property {function() :void} funcHere
+ * @property {function(CSSelector) :void} orFuncHere
+ * @property {function(CSSelector) :void} evenFuncHere
+ */
+
+
+/** @type {ExampleModule} */
+const theModule = Object.freeze({
+	funcHere: someFunc,
+	orFuncHere: otherFunc,
+	evenFuncHere: anotherFunc
+});
+
+export default theModule;
