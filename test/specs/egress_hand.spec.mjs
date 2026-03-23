69410eb1dfd9a274f9697273300742e17d406977
Barry Harris <bharrisaplus@gmail.com>
2026-03-23 17:51:14 -0400

    Update egress spec with test cases for error handling around image generation

--- test/specs/egress_hand.spec.mjs
+++ test/specs/egress_hand.spec.mjs
@@ -145,7 +145,6 @@ test("pcs:hand:egress:generateImage should run without issue", async (swear) => 
 		swearImg = td.constructor(linkeImg),
 		swearCanvas = td.constructor(swearDoc.HTMLCanvasElement),
 		swearCanvasInst = td.instance(swearCanvas),
-		swear2DCtx = td.object(['drawImage']),
 
 		/** @type {Hand.Egress} */
 		egressHand = await getImport(
@@ -154,14 +153,13 @@ test("pcs:hand:egress:generateImage should run without issue", async (swear) => 
 
 
 	swearImg.prototype.decode = td.func();
-	swearCanvas.prototype.getContext = td.func();
-	swearCanvas.prototype.toDataURL = td.func();
+	swearCanvasInst.getContext = td.func();
+	swearCanvasInst.toDataURL = td.func();
 	td.when(swearCanvas(0, 0)).thenReturn(swearCanvasInst);
-	td.when(swearCanvas.prototype.getContext('2d')).thenReturn(swear2DCtx);
+	td.when(swearCanvasInst.getContext('2d')).thenReturn(td.object(['drawImage']));
 	td.when(swearXMLSerializer(td.matchers.anything())).thenReturn(swearSVG.toWellFormed());
 	td.when(swearImg.prototype.decode()).thenResolve(undefined);
-	td.when(swear2DCtx.drawImage(td.matchers.anything())).thenReturn(null);
-	td.when(swearCanvas.prototype.toDataURL()).thenReturn("12d34");
+	td.when(swearCanvasInst.toDataURL()).thenReturn("12d34");
 
 	await egressHand.generateImage("#333", swearSpriteList, `#${swearSVGSelector}`);
 	
@@ -176,4 +174,178 @@ test("pcs:hand:egress:generateImage should run without issue", async (swear) => 
 	swear.isEqual(bonafiedExplntns[1].callCount, 1, "Should generate image via data url");
 	swear.isEqual(swearCanvasInst.width, 1000, "Should create canvas and set width");
 	swear.isEqual(swearCanvasInst.height, 400, "Should create canvas and set height");
+});
+
+
+test("pcs:hand:egress:generateImage should have issues", async (swear) => {
+	let bonafiedResults = [], bonafiedExplntns = [];
+	const
+		swearScribe = td.object(['issuelog']),
+		swearXMLSerializer = td.constructor(['serializeToString']),
+		swearClipboard = td.object(),
+		swearSpriteLists = [
+			Array.from({length: 51}, (_, _idx) => { return `#${_idx}`; }),
+			Array.from({length: 52}, (_, _idx) => { return `#${_idx}`; }),
+		],
+		swearSVGSelectors = [
+			'test-vector',
+			'test-vectol'
+		],
+		swearSVG = `<svg id="${swearSVGSelectors[0]}"><defs><symbol><rect></rect></symbol></defs></svg>`,
+		swearHTML = `<!doctype html><html lang="en"><body>${swearSVG}</body></html>`,
+		{
+			Image: linkeImg,
+			document: swearDoc,
+			window: swearWindow
+		} = linkeParse(swearHTML),
+		swearImg = td.constructor(linkeImg),
+		swearCanvas = td.constructor(swearDoc.HTMLCanvasElement),
+		swearCanvasInst = td.instance(swearCanvas),
+
+		/** @type {Hand.Egress} */
+		egressHand = await getImport(
+			swearScribe, swearWindow, swearDoc, swearXMLSerializer, swearClipboard, swearImg, swearCanvas
+		);
+
+
+	swearImg.prototype.decode = td.func();
+	swearCanvasInst.getContext = td.func();
+	swearCanvasInst.toDataURL = td.func();
+	td.when(swearCanvas(0, 0)).thenReturn(swearCanvasInst);
+	td.when(swearCanvasInst.getContext('2d')).thenReturn(td.object(['drawImage']));
+
+	bonafiedResults.push(
+		await egressHand.generateImage("#333", swearSpriteLists[0], `#${swearSVGSelectors[0]}`)
+	);
+
+	bonafiedResults.push(
+		await egressHand.generateImage("#333", swearSpriteLists[1], `#${swearSVGSelectors[1]}`)
+	);
+
+	bonafiedExplntns.push(td.explain(swearScribe.issuelog));
+	bonafiedExplntns.push(td.explain(swearCanvasInst.toDataURL));
+
+	td.reset();
+
+
+	swear.plan(5);
+	swear.isEqual(bonafiedExplntns[0].callCount, 2, "Should have issues");
+	swear.isEqual(bonafiedExplntns[1].callCount, 0, "Should generate no image");
+	swear.isEqual(bonafiedResults.join(""), "", "Should generate no image");
+	swear.isEqual(bonafiedExplntns[0].calls[0].cloneArgs[0], "Missing components for image download",
+		"Should log expected issue"
+	);
+	swear.isEqual(bonafiedExplntns[0].calls[1].cloneArgs[0], "Missing components for image download",
+		"Should log expected issue"
+	);
+});
+
+
+test("pcs:hand:egress:generateImage should have issues (cont)", async (swear) => {
+	let bonafiedResults = [], bonafiedExplntns = [];
+	const
+		swearScribe = td.object(['issuelog']),
+		swearXMLSerializer = td.constructor(['serializeToString']),
+		swearClipboard = td.object(),
+		swearSpriteList = Array.from({length: 52}, (_, _idx) => { return `#${_idx}`; }),
+		swearSVGSelector = 'test-vector',
+		swearSVG = `<svg id="${swearSVGSelector}"><defs><symbol><rect></rect></symbol></defs></svg>`,
+		swearHTML = `<!doctype html><html lang="en"><body>${swearSVG}</body></html>`,
+		{
+			Image: linkeImg,
+			document: swearDoc,
+			window: swearWindow
+		} = linkeParse(swearHTML),
+		swearImg = td.constructor(linkeImg),
+		swearCanvas = td.constructor(swearDoc.HTMLCanvasElement),
+		swearCanvasInst = td.instance(swearCanvas),
+
+		/** @type {Hand.Egress} */
+		egressHand = await getImport(
+			swearScribe, swearWindow, swearDoc, swearXMLSerializer, swearClipboard, swearImg, swearCanvas
+		);
+
+
+	swearImg.prototype.decode = td.func();
+	swearCanvasInst.getContext = td.func();
+	swearCanvasInst.toDataURL = td.func();
+	td.when(swearCanvas(0, 0)).thenReturn(swearCanvasInst);
+	td.when(swearCanvasInst.getContext('2d')).thenReturn(td.object(['drawImage']));
+	td.when(swearXMLSerializer(td.matchers.anything())).thenReturn(swearSVG.toWellFormed());
+
+	td.when(swearCanvasInst.toDataURL()).thenThrow(new DOMException('darn', 'SecurityError'));
+	bonafiedResults.push(
+		await egressHand.generateImage("#333", swearSpriteList, `#${swearSVGSelector}`)
+	);
+
+	td.when(swearImg.prototype.decode()).thenThrow(new DOMException('oops', 'EncodingError'));
+	bonafiedResults.push(
+		await egressHand.generateImage("#333", swearSpriteList, `#${swearSVGSelector}`)
+	);
+
+
+	bonafiedExplntns.push(td.explain(swearScribe.issuelog));
+	bonafiedExplntns.push(td.explain(swearCanvasInst.toDataURL));
+
+	td.reset();
+
+
+	swear.plan(5);
+	swear.isEqual(bonafiedExplntns[0].callCount, 2, "Should have issues");
+	swear.isEqual(bonafiedExplntns[1].callCount, 1, "Should attempt to generate image once");
+	swear.isEqual(bonafiedResults.join(""), "", "Should generate no image");
+	swear.isEqual(bonafiedExplntns[0].calls[0].cloneArgs[0], "Issue with canvas", "Should log expected issue");
+	swear.isEqual(bonafiedExplntns[0].calls[1].cloneArgs[0], "Issue with image decode",
+		"Should log expected issue"
+	);
+});
+
+test("pcs:hand:egress:generateImage should have issues (cont'd)", async (swear) => {
+	let bonafiedResult, bonafiedExplntns = [];
+	const
+		swearScribe = td.object(['issuelog']),
+		swearXMLSerializer = td.constructor(['serializeToString']),
+		swearClipboard = td.object(),
+		swearSpriteList = Array.from({length: 52}, (_, _idx) => { return `#${_idx}`; }),
+		swearSVGSelector = 'test-vector',
+		swearSVG = `<svg id="${swearSVGSelector}"><defs><symbol><rect></rect></symbol></defs></svg>`,
+		swearHTML = `<!doctype html><html lang="en"><body>${swearSVG}</body></html>`,
+		{
+			Image: linkeImg,
+			document: swearDoc,
+			window: swearWindow
+		} = linkeParse(swearHTML),
+		swearImg = td.constructor(linkeImg),
+		swearCanvas = td.constructor(swearDoc.HTMLCanvasElement),
+		swearCanvasInst = td.instance(swearCanvas),
+		swear2DCTX = { drawImage: () => { throw new DOMException('welp', 'BadThing') } },
+
+		/** @type {Hand.Egress} */
+		egressHand = await getImport(
+			swearScribe, swearWindow, swearDoc, swearXMLSerializer, swearClipboard, swearImg, swearCanvas
+		);
+
+
+	swearImg.prototype.decode = td.func();
+	swearCanvasInst.toDataURL = td.func();
+	swearCanvasInst.getContext = () => { return swear2DCTX; };
+	td.when(swearCanvas(0, 0)).thenReturn(swearCanvasInst);
+	td.when(swearCanvasInst.getContext('2d')).thenReturn(swear2DCTX);
+	td.when(swearXMLSerializer(td.matchers.anything())).thenReturn(swearSVG.toWellFormed());
+
+	bonafiedResult = await egressHand.generateImage("#333", swearSpriteList, `#${swearSVGSelector}`);
+	
+	bonafiedExplntns.push(td.explain(swearScribe.issuelog));
+	bonafiedExplntns.push(td.explain(swearCanvasInst.toDataURL));
+
+	td.reset();
+
+
+	swear.plan(4);
+	swear.isEqual(bonafiedExplntns[0].callCount, 1, "Should have issue");
+	swear.isEqual(bonafiedExplntns[1].callCount, 0, "Should generate no image");
+	swear.isEqual(bonafiedResult, "", "Should generate no image");
+	swear.isEqual(bonafiedExplntns[0].calls[0].cloneArgs[0], "Issue with web API",
+		"Should log expected issue"
+	);
 });
