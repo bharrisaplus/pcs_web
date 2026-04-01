0844b042902af614b58583c4dba7cfbe667b10a1
Barry Harris <bharrisaplus@gmail.com>
2026-03-31 21:16:46 -0400

    Hack dom types to allow CustomEvent object to be passed to addEventListener handlers

--- source/behavior/_meta/_.d.ts
+++ source/behavior/_meta/_.d.ts
@@ -1,3 +1,4 @@
+// oxlint-disable @stylistic/max-len
 
 /**
  * Type definitions for using ChanceJS
@@ -5,9 +6,16 @@
  */
 interface chanceJS {
   pickset<T>(arr :T[], count ?:number) :T[];
-}
+};
 
 declare global {
+  // Tweaks for lib.dom.d.ts
+  interface CustomEventListener { (evt: CustomEvent): void; }
+  type EventListenerOrCustomEventListenerOrEventListenerObject = EventListener | CustomEventListener | EventListenerObject;
+  interface HTMLElement extends Element, ElementCSSInlineStyle, ElementContentEditable, GlobalEventHandlers, HTMLOrSVGElement {
+    addEventListener(type: string, listener: EventListenerOrCustomEventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+  }
+
   const chance :chanceJS;
 }
 
