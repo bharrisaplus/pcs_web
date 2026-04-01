c6e1079d979bf73c013d0134e76ce4f5fdae9336
Barry Harris <bharrisaplus@gmail.com>
2026-04-01 06:31:49 -0400

    Adjust spacing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,23 +3,23 @@ import { default as NodeProcess } from 'node:process';
 let targetConfig;
 
 if (NodeProcess.env.BUILD_TARGET == 'octocat') {
-	const octocatCurrent = await import('./build/octocat/_rollup.config.mjs');
+  const octocatCurrent = await import('./build/octocat/_rollup.config.mjs');
 
-	targetConfig = octocatCurrent.default;
+  targetConfig = octocatCurrent.default;
 } else if (NodeProcess.env.BUILD_TARGET == 'soi') {
-	switch(NodeProcess.env.BUILD_OBJECTIVE) {
-		case 'demo': {
-			const octocatV0 = await import('./build/demo/_rollup.config.mjs');
+  switch(NodeProcess.env.BUILD_OBJECTIVE) {
+    case 'demo': {
+      const octocatV0 = await import('./build/demo/_rollup.config.mjs');
 
-			targetConfig = octocatV0.default;
-			break;
-		}
-		default: {
-			const soiCurrent = await import('./build/_rollup.config.mjs');
+      targetConfig = octocatV0.default;
+      break;
+    }
+    default: {
+      const soiCurrent = await import('./build/_rollup.config.mjs');
 
-			targetConfig = soiCurrent.default;
-		}
-	}
+      targetConfig = soiCurrent.default;
+    }
+  }
 }
 
 export default targetConfig;
