896869da61171ab5abe0a5a66f849fd4273500aa
Barry Harris <bharrisaplus@gmail.com>
2026-04-02 00:50:27 -0400

    Add transpile step to harness test server

--- test/stories/tankoban.mjs
+++ test/stories/tankoban.mjs
@@ -1,11 +1,14 @@
-import { argv as NodeArgParse } from 'node:process';
+import { argv as NodeArgParse, exit as NodeExit } from 'node:process';
 import { default as NodePath } from 'node:path';
 import { default as NodeFS } from 'node:fs/promises';
 import http from 'node:http';
 import { parseHTML as linkeParse } from 'linkedom';
 import { html as $ } from 'ucontent';
+import { renderFile as pugRender } from 'pug';
+import { render as stylRender } from 'stylus';
 
-let foundPreface, isVerbose, cssReset, prefaceTD, faviconIco;
+
+let _stylTmp;
 const
   _dir = import.meta.dirname,
 
@@ -19,105 +22,178 @@ const
 
   okRootPaths = ['/', '/index.html', '/index', '/tankoban.html', '/tankoban'],
 
-  baseHTML = $`
-  <!DOCTYPE html><html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Tankoban</title>
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="/reset.css"/>
-      <script type="text/javascript" src="/td.mjs"></script>
-    </head>
-    <body>
-    <div id="hookhere"></div>
-    <script type="module" defer>
-      let result;
-      const abc = td.object(['hello']);
+  okConte = [
+    'turntable_part'
+  ],
 
-      td.when(abc.hello()).thenReturn('world');
+  coveragePathPrefix = '/source/behavior',
 
-      result = abc.hello();
+  { document: rootDoc } = linkeParse($`
+    <!DOCTYPE html><html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Tankoban</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/reset.css"/>
+        <script type="text/javascript" src="/td.mjs"></script>
+      </head>
+      <body>
+      <div id="hookhere"></div>
+      <script type="module" defer>
+        let result;
+        const abc = td.object(['hello']);
 
-      console.debug(td.explain(abc.hello));
-      console.info(result);
-    </script>
-    </body>
-  </html>
-`,
+        td.when(abc.hello()).thenReturn('world');
 
-{ document: rootDoc } = linkeParse(baseHTML);
+        result = abc.hello();
+
+        console.debug(td.explain(abc.hello));
+        console.info(result);
+      </script>
+      </body>
+    </html>
+  `),
+
+  isVerbose = NodeArgParse[2] == '-v' || NodeArgParse[2] == '--verbose',
+  foundPreface = requiredFiles.every(async (reqFl) => await NodeFS.stat(NodePath.resolve(_dir, reqFl))),
+  cssReset = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[3]), { encoding: 'utf8' }),
+  faviconIco = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[4])),
+  prefaceTD = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[2]), { encoding: 'utf8' });
 
 
-isVerbose = NodeArgParse[2] == '-v' || NodeArgParse[2] == '--verbose';
-foundPreface = requiredFiles.every(async (reqFl) => await NodeFS.stat(NodePath.resolve(_dir, reqFl)));
-cssReset = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[3]), { encoding: 'utf8' });
-faviconIco = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[4]));
-prefaceTD = await NodeFS.readFile(NodePath.resolve(_dir, requiredFiles[2]));
+const headerForMime = (dotExt = '') => {
+  let result;
+  switch(dotExt) {
+    case '.js':
+    case '.mjs': result = { 'Content-Type': 'text/javascript' }; break;
+    case '.css': result = { 'Content-Type': 'text/css' }; break;
+    case '.svg': result = { 'Content-Type': 'image/svg+xml' }; break;
+    case '.html': result = { 'Content-Type': 'text/html' }; break;
+    case '.json': result = { 'Content-Type': 'application/json' }; break;
+    case '.ico': result = { 'Content-Type': 'image/x-icon' }; break;
+    default: result = { 'Content-Type': 'text/plain' }
+  }
 
-if (isVerbose) { console.log(`Found preface files: ${foundPreface}`); }
+  return result;
+};
 
-if (foundPreface) {
-  const TankoBanServer = http.createServer(async (req, res) => {
-    let
-      isCSSReset = false,
-      isFavicon = false,
-      isTDJS = false,
-      resCode,
-      resHeader;
-    const $greetElement = rootDoc.createElement('h1');
 
-    if (req.url) {
-      if(okRootPaths.indexOf(req.url) != -1) {
-        $greetElement.innerHTML = $`<h1>Hello</h1>`;
-        resHeader = { 'Content-Type': 'text/html' };
+const TankoBanServer = http.createServer(async (req, res) => {
+  let
+    isCSSReset = false,
+    isFavicon = false,
+    isTDJS = false,
+    foundContent = false,
+    lookupUrl = req.url || '',
+    lookupExt,
+    lookupContent,
+    resCode,
+    resHeader;
+  const $greetElement = rootDoc.createElement('h1');
+
+  if (lookupUrl) {
+    if(okRootPaths.indexOf(lookupUrl) != -1) {
+      $greetElement.textContent = $`Hello`;
+      resHeader = headerForMime('.html');
+      resCode = 200;
+    } else if (lookupUrl == "/reset.css") {
+      resHeader = headerForMime('.css');
+      resCode = 200;
+      isCSSReset = true;
+    } else if (lookupUrl == "/main.css") {
+      resHeader = headerForMime('.css');
+      resCode = 200;
+    } else if (lookupUrl == '/favicon.ico' || NodePath.extname(lookupUrl) == '.ico') {
+      resHeader = headerForMime('.ico');
+      resCode = 200;
+      isFavicon = true;
+    } else if (lookupUrl == '/td.mjs') {
+      resHeader = headerForMime('.mjs');
+      resCode = 200;
+      isTDJS = true;
+    } else {
+      if (okConte.some((_conte) => lookupUrl.startsWith(`/${_conte}`))) {
+        lookupExt = NodePath.extname(lookupUrl);
+
+        try {
+          if (lookupExt == '') {
+            lookupContent = pugRender(NodePath.resolve(_dir, `./single/${lookupUrl}/panel.pug`));
+            resHeader = headerForMime('.html');
+          } else {
+            if (['.js','.mjs','.svg','.json'].indexOf(lookupExt) != -1) {
+              lookupContent = await NodeFS.readFile(
+                NodePath.resolve(_dir, `./single/${lookupUrl}`), { encoding: 'utf8' }
+              );
+              resHeader = headerForMime(lookupExt);
+            } else if (lookupExt == '.css') {
+              _stylTmp = await NodeFS.readFile(
+                NodePath.resolve(
+                  _dir,`./single/${NodePath.dirname(lookupUrl)}/${NodePath.basename(lookupUrl, '.css')}.styl`
+                ),
+                { encoding: 'utf8' }
+              );
+              lookupContent = stylRender(_stylTmp, {paths: [NodePath.resolve(_dir)]});
+              resHeader = headerForMime('.css');
+            } else {
+              lookupContent = await NodeFS.readFile(NodePath.resolve(_dir, `./${lookupUrl}`));
+              resHeader = headerForMime(lookupExt);
+            }
+          }
+
+          resCode = 200;
+          foundContent = true;
+        } catch (lookupErr) {
+          console.error(lookupErr);
+          lookupContent = '';
+          $greetElement.textContent = `Not Found`;
+          resHeader = headerForMime('.html');
+          resCode = 404;
+        }
+      } else if (lookupUrl.startsWith(coveragePathPrefix)) {
+        $greetElement.textContent = `Hello`;
+        resHeader = headerForMime('.mjs');
         resCode = 200;
-      } else if (req.url == "/reset.css") {
-        resHeader = { 'Content-Type': 'text/css' };
-        resCode = 200;
-        isCSSReset = true;
-      } else if (req.url == '/favicon.ico') {
-        resHeader = { 'Content-Type': 'image/x-icon' };
-        resCode = 200;
-        isFavicon = true;
-      } else if (req.url == '/td.mjs') {
-        resHeader = { 'Content-Type': 'text/javascript'};
-        resCode = 200;
-        isTDJS = true;
+        foundContent = true;
       } else {
-        $greetElement.innerHTML = $`<h1>Not Found</h1>`;
-        resHeader = { 'Content-Type': 'text/html' };
+        $greetElement.textContent = `Not Found`;
+        resHeader = headerForMime('.html');
         resCode = 404;
       }
-    } else {
-      $greetElement.innerHTML = $`<h1>Not Found</h1>`;
-      resHeader = { 'Content-Type': 'text/html' }
-      resCode = 404;
     }
+  } else {
+    $greetElement.textContent = `Not Found`;
+    resHeader = headerForMime('.html');
+    resCode = 404;
+  }
 
-    if (isVerbose) {
-      console.log("Responding to");
-      console.debug(req.url);
-    }
+  if (isVerbose) {
+    console.debug(`Responding to: ${lookupUrl}`);
+  }
 
-    res.writeHead(resCode, resHeader);
+  res.writeHead(resCode, resHeader);
 
-    if (isFavicon) {
-      res.write(faviconIco);
-    } else if (isCSSReset) {
-      res.write(cssReset);
-    } else if (isTDJS) {
-      res.write(prefaceTD);
-    } else {
+  switch(true) {
+    case isFavicon: res.write(faviconIco); break;
+    case isCSSReset: res.write(cssReset); break;
+    case isTDJS: res.write(prefaceTD); break;
+    case foundContent: res.write(lookupContent); break;
+    default: {
       rootDoc.querySelector('#hookhere')?.replaceChildren($greetElement);
       res.write(rootDoc.toString());
     }
+  }
 
-    res.end();
-  });
+  res.end();
+});
 
-  console.log("Listening on 54321...");
-  TankoBanServer.listen(54321);
-} else {
+
+// Start
+
+if (!foundPreface) {
   console.error(`Preface files not found - ensure files located relative to server:`);
   console.debug(requiredFiles.toString());
+  NodeExit(1);
 }
+
+console.log("Listening on 54321...");
+TankoBanServer.listen(54321);
