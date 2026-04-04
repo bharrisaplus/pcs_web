51f171d86f411db5e24793e76afff40d40f06d17
Barry Harris <bharrisaplus@gmail.com>
2026-04-04 01:01:45 -0400

    Wire up test subject for popover to trigger toggle from parent window

--- test/stories/single/turntable_part/_tankoban.mjs
+++ test/stories/single/turntable_part/_tankoban.mjs
@@ -1,6 +1,32 @@
 
+let loadedSubject = false;
+/** @type {HTMLButtonElement} */
+const $subjToggleBtn = document.createElement('button');
+
+
+$subjToggleBtn.setAttribute('id', 'showhide');
+
 window.addEventListener('message', (_msgEvt) => {
-  if (_msgEvt.data.type == 'loaded') {
-    console.log("Loaded subject");
-  }
+  if (_msgEvt.data.type != 'loaded') { return; }
+  if (loadedSubject) { return; }
+
+  loadedSubject = true;
+  $subjToggleBtn.textContent = "Show/Hide Subject"
+  document.querySelector('#turntable-panel')?.appendChild($subjToggleBtn);
+}, { once: true });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  $subjToggleBtn.addEventListener('click', (_clickEvt) => {
+    if (_clickEvt.target != $subjToggleBtn) { return; }
+    if (!loadedSubject) { return; }
+
+    $subjToggleBtn.disabled = true;
+
+    window.frames[0].focus();
+    window.frames[0].postMessage({ type: 'subjectToggle' }, '*');
+    window.setTimeout(() => {
+      $subjToggleBtn.disabled = false;
+    }, 1000);
+  });
 });
--- test/stories/single/turntable_part/conte.mjs
+++ test/stories/single/turntable_part/conte.mjs
@@ -1,4 +1,12 @@
+/** @type {HTMLElement} */
+const turntableSubject = document.querySelector('#turntable');
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('message', (_msgEvt) => {
+    if (_msgEvt.data.type != 'subjectToggle') { return; }
+
+    turntableSubject.togglePopover();
+  });
+
   window.parent.postMessage({type: 'loaded'}, '*');
 });
