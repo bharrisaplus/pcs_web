1a9b1a0570e6c47210c42cdb73af71fba0d953f0
Barry Harris <bharrisaplus@gmail.com>
2026-04-09 10:36:05 -0400

    Update rollup config to replace autostart line in test runner

--- test/_rollup.config.mjs
+++ test/_rollup.config.mjs
@@ -6,6 +6,7 @@ import { nodeResolve as RollupNodeResolve } from '@rollup/plugin-node-resolve';
 import { default as RollupCommonJS } from '@rollup/plugin-commonjs';
 import { default as RollupJSON } from '@rollup/plugin-json';
 import { default as RollupNodePolyfills } from 'rollup-plugin-node-polyfills';
+import { default as RollupReplace } from '@rollup/plugin-replace';
 
 import { default as testShared } from './compass.mjs'
 
@@ -27,6 +28,24 @@ if (NodeProcess.env.BUILD_TARGET == "story_preface:testdouble") {
       RollupCommonJS({transformMixedEsModules: true, defaultIsModuleExports: true}),
       RollupJSON(),
       RollupNodePolyfills()
+    ]
+  };
+} else if (NodeProcess.env.BUILD_TARGET == "story_preface:zora") {
+  targetConfig = {
+    input: 'zora_browser',
+    output: {
+      file: NodePath.resolve(testShared.story_path, './_z.mjs'),
+      format: 'es'
+    },
+    plugins: [
+      RollupVirtual({
+        zora_browser: `export * from 'zora';`
+      }),
+      RollupNodeResolve({browser: true}),
+      RollupReplace({
+        preventAssignment: false,
+        'let autoStart = true;': 'let autoStart = false;'
+      })
     ]
   };
 };
