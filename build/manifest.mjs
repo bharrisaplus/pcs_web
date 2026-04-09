8ed7936a78befd191fda1df535c74b192113c9c8
Barry Harris <bharrisaplus@gmail.com>
2026-04-09 06:35:02 -0400

    Add type for shared build variables

--- build/manifest.mjs
+++ build/manifest.mjs
@@ -4,7 +4,8 @@ import { default as NodePath } from 'node:path';
 
 const
   _dir = import.meta.dirname,
-   milieu = {
+  /** @type {BuildGlobals} */
+  milieu = {
     project_path: NodeProcess.env.PROJECT_DIR || NodePath.resolve(_dir, '../'),
     behavior_path: NodeProcess.env.BEHAVIOR_DIR || NodePath.resolve(_dir, '../source/behavior'),
     demo_path: NodeProcess.env.DEMO_DIR || NodePath.resolve(_dir, '../distribution/demo'),
--- source/behavior/_meta/_.d.ts
+++ source/behavior/_meta/_.d.ts
@@ -10,12 +10,25 @@ interface chanceJS {
 
 declare global {
   // Tweaks for lib.dom.d.ts
-  interface CustomEventListener { (evt: CustomEvent): void; }
+  interface CustomEventListener { (evt: CustomEvent): void; };
   type EventListenerOrCustomEventListenerOrEventListenerObject = EventListener | CustomEventListener | EventListenerObject;
   interface HTMLElement extends Element, ElementCSSInlineStyle, ElementContentEditable, GlobalEventHandlers, HTMLOrSVGElement {
     addEventListener(type: string, listener: EventListenerOrCustomEventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
-  }
+  };
 
+  // See /build/manifest.mjs
+  type BuildGlobals = {
+    project_path: string;
+    behavior_path: string;
+    demo_path: string;
+    dev_path: string;
+    octocat_path: string;
+    filehost_url: string;
+    localhost_url: string;
+    es_main: string;
+  };
+
+  // ChanceJS browser global
   const chance :chanceJS;
 }
 
