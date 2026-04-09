2fe088d280d58a3d59ef769d16d891e2748c3075
Barry Harris <bharrisaplus@gmail.com>
2026-04-09 12:15:02 -0400

    Update test script to import test runner module and add function for running test on window message

--- test/stories/single/turntable_part/conte.mjs
+++ test/stories/single/turntable_part/conte.mjs
@@ -3,9 +3,13 @@
  */
 
 /* This files imports should be specified as part of the importmap in subject.page.pug */
+import { default as td } from 'testdouble';
+import { hold as zaHold, test as zaTest, report as zaReport, createTAPReporter } from 'zora';
+
 import { default as _tg } from './clones/_glods.clone.mjs';
 import { default as getTurntable } from 'turntable_part';
 
+zaHold();
 
 const
   turntableID = '#turntable',
@@ -15,7 +19,24 @@ const
   turntableBehavior = getTurntable(turntableID),
   logGlobals = () => { console.debug(_tg); },
   /** @type {CardIntri[]} */
-  testCards = [];
+  testCards = [],
+  zaTapReporter = createTAPReporter();
+
+
+const run_tests = () => {
+  zaTest('Should be ok', (z) => {
+    let testResult, testExplntn;
+    const mockObj = td.object(['hello']);
+
+    td.when(mockObj.hello()).thenReturn('world');
+
+    testResult = mockObj.hello();
+    testExplntn = td.explain(mockObj.hello);
+
+    z.same(testResult, 'world', "Should return world from hello");
+    z.same(testExplntn.callCount, 1, "Should call hello once");
+  });
+};
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -46,6 +67,10 @@ document.addEventListener('DOMContentLoaded', () => {
         turntableBehavior.loadTurntable(testCards[0]);
       } else if (_msgEvt.data.type == 'subject:hide') {
         $turntable.hidePopover();
+      } else if (_msgEvt.data.type == 'subject:test') {
+        $turntable.hidePopover();
+        run_tests();
+        zaReport({ reporter: zaTapReporter });
       } else {
         console.warn(`Received unknown msg type: ${_msgEvt.data.type}`);
       }
