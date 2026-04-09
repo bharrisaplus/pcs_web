c6ef6a428b4311b3cb385a225d55f0579a08443c
Barry Harris <bharrisaplus@gmail.com>
2026-04-09 15:38:49 -0400

    Add button to panel for run a11y checks with axe

--- test/stories/single/turntable_part/_tankoban.mjs
+++ test/stories/single/turntable_part/_tankoban.mjs
@@ -11,6 +11,8 @@ const
   $subjLoad = document.createElement('button'),
   /** @type {HTMLButtonElement} */
   $subjRun = document.createElement('button'),
+  /** @type {HTMLButtonElement} */
+  $subja11y = document.createElement('button'),
   /** @type {HTMLElement} */
   $overlay = document.querySelector('#test-overlay');
 
@@ -19,6 +21,7 @@ $subjShow.setAttribute('id', 'show');
 $subjHide.setAttribute('id', 'hide');
 $subjLoad.setAttribute('id', 'loadone');
 $subjRun.setAttribute('id', 'runtest');
+$subja11y.setAttribute('id', 'a11ycheck');
 
 window.addEventListener('message', (_msgEvt) => {
   if (_msgEvt.data.type == 'loaded') {
@@ -29,10 +32,12 @@ window.addEventListener('message', (_msgEvt) => {
     $subjHide.textContent = "Hide";
     $subjLoad.textContent = "Load Cards";
     $subjRun.textContent = "Run Test";
+    $subja11y.textContent = "Check a11y";
     document.querySelector('#ctrl-band')?.appendChild($subjShow);
     document.querySelector('#ctrl-band')?.appendChild($subjHide);
     document.querySelector('#ctrl-band')?.appendChild($subjLoad);
     document.querySelector('#ctrl-band')?.appendChild($subjRun);
+    document.querySelector('#ctrl-band')?.appendChild($subja11y);
     document.querySelector('#ctrl-band')?.classList.remove('load-curtain');
     window.printTestGlobals = function () { window.frames[0].postMessage({ type: 'print:globals'}); };
   } else if (_msgEvt.data.type == 'finished') {
@@ -42,6 +47,7 @@ window.addEventListener('message', (_msgEvt) => {
     $subjHide.disabled = false;
     $subjShow.disabled = false;
     $subjLoad.disabled = false;
+    $subja11y.disabled = false;
 
     $overlay.classList.remove('lift');
   } else { return; }
@@ -97,9 +103,23 @@ document.addEventListener('DOMContentLoaded', () => {
     $subjHide.disabled = true;
     $subjShow.disabled = true;
     $subjLoad.disabled = true;
+    $subja11y.disabled = true;
 
     $overlay.classList.add('lift');
     window.frames[0].focus();
     window.frames[0].postMessage({ type: 'subject:test' });
+  });
+
+  $subja11y.addEventListener('click', (_clickEvt) => {
+    if (_clickEvt.target != $subja11y) { return; }
+    if (!loadedSubject) { return; }
+
+    $subja11y.disabled = true;
+
+    window.frames[0].focus();
+    window.frames[0].postMessage({ type: 'subject:a11y' });
+    window.setTimeout(() => {
+      $subja11y.disabled = false;
+    }, 1250);
   });
 });
