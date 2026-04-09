529284e06af97ba639ed7001bfc364d9eba720c0
Barry Harris <bharrisaplus@gmail.com>
2026-04-09 13:18:04 -0400

    Add overlay and button to test panel for running test

--- test/stories/single/turntable_part/_tankoban.mjs
+++ test/stories/single/turntable_part/_tankoban.mjs
@@ -1,33 +1,51 @@
 
-let loadedSubject = false;
-
+let
+  loadedSubject = false,
+  runningTest = false;
 const
   /** @type {HTMLButtonElement} */
   $subjShow = document.createElement('button'),
   /** @type {HTMLButtonElement} */
   $subjHide = document.createElement('button'),
     /** @type {HTMLButtonElement} */
-  $subjLoad = document.createElement('button');
+  $subjLoad = document.createElement('button'),
+  /** @type {HTMLButtonElement} */
+  $subjRun = document.createElement('button'),
+  /** @type {HTMLElement} */
+  $overlay = document.querySelector('#test-overlay');
 
 
 $subjShow.setAttribute('id', 'show');
 $subjHide.setAttribute('id', 'hide');
 $subjLoad.setAttribute('id', 'loadone');
+$subjRun.setAttribute('id', 'runtest');
 
 window.addEventListener('message', (_msgEvt) => {
-  if (_msgEvt.data.type != 'loaded') { return; }
-  if (loadedSubject) { return; }
+  if (_msgEvt.data.type == 'loaded') {
+    if (loadedSubject) { return; }
 
-  loadedSubject = true;
-  $subjShow.textContent = "Show"
-  $subjHide.textContent = "Hide"
-  $subjLoad.textContent = "Load Cards"
-  document.querySelector('#ctrl-band')?.appendChild($subjShow);
-  document.querySelector('#ctrl-band')?.appendChild($subjHide);
-  document.querySelector('#ctrl-band')?.appendChild($subjLoad);
-  document.querySelector('#ctrl-band')?.classList.remove('load-curtain');
-  window.printTestGlobals = function () { window.frames[0].postMessage({ type: 'print:globals'}); };
-}, { once: true });
+    loadedSubject = true;
+    $subjShow.textContent = "Show";
+    $subjHide.textContent = "Hide";
+    $subjLoad.textContent = "Load Cards";
+    $subjRun.textContent = "Run Test";
+    document.querySelector('#ctrl-band')?.appendChild($subjShow);
+    document.querySelector('#ctrl-band')?.appendChild($subjHide);
+    document.querySelector('#ctrl-band')?.appendChild($subjLoad);
+    document.querySelector('#ctrl-band')?.appendChild($subjRun);
+    document.querySelector('#ctrl-band')?.classList.remove('load-curtain');
+    window.printTestGlobals = function () { window.frames[0].postMessage({ type: 'print:globals'}); };
+  } else if (_msgEvt.data.type == 'finished') {
+    if (!runningTest) { return; }
+
+    $subjRun.disabled = false;
+    $subjHide.disabled = false;
+    $subjShow.disabled = false;
+    $subjLoad.disabled = false;
+
+    $overlay.classList.remove('lift');
+  } else { return; }
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -68,5 +86,18 @@ document.addEventListener('DOMContentLoaded', () => {
     window.setTimeout(() => {
       $subjLoad.disabled = false;
     }, 1000);
+  });
+
+  $subjRun.addEventListener('click', (_clickEvt) => {
+    if (_clickEvt.target != $subjRun) { return; }
+    if (!loadedSubject) { return; }
+
+    runningTest = true;
+    $subjRun.disabled = true;
+    $subjHide.disabled = true;
+    $subjShow.disabled = true;
+    $subjLoad.disabled = true;
+
+    $overlay.classList.add('lift');
   });
 });
