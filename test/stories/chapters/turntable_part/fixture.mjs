25eca70b165723d066c6637d792ff8315b6d6bb7
Barry Harris <bharrisaplus@gmail.com>
2026-04-10 17:29:12 -0400

    Add fixture module for test subject script.

---
+++ test/stories/chapters/turntable_part/fixture.mjs
@@ -0,0 +1,62 @@
+import { hold as zaHold, test as zaTest, report as zaReport, createTAPReporter } from 'zora';
+
+import { default as _tg } from './clones/_glods.clone.mjs';
+
+
+/** @type {string[]} */
+let zaTapLines = [];
+const
+  zaTapLogger = (/** @type {string} */ zaTapLogLine) => { zaTapLines.push(zaTapLogLine); },
+  zaTapSerializer = (/** @type {any} */ zaTapVal) => { return JSON.stringify(zaTapVal); },
+  zaTapReporter = createTAPReporter({ log: zaTapLogger, serialize: zaTapSerializer });
+
+
+const get_bound_idx = (cur = 0) => {
+  let result;
+
+  switch (true) {
+    case (cur >= _tg.c_Max): result = _tg.c_Max - 1; break;
+    case (cur < 0): result = 0; break;
+    default: result = cur;
+  }
+
+  return result;
+};
+
+
+/**
+ * @param  {Function} runFnc
+ */
+const test_routine = (runFnc) => {
+  runFnc();
+
+  zaReport({ reporter: zaTapReporter }).then(() => {
+    window.__tap__ = zaTapLines.join('\n');
+    window.parent.postMessage({type: 'finished'});
+    zaTapLines = [];
+  }, (rejReason) => {
+    console.warn("issue with zora reporter");
+    console.debug(rejReason)
+  });
+};
+
+const getFixtures = () => {
+  return Object.freeze({
+    run_tests: test_routine,
+    chk: zaTest,
+    sanitize_card_id: get_bound_idx
+  });
+};
+
+
+const apparatus = getFixtures();
+
+zaHold();
+window.axe.configure({
+  rules: [ // Just testing a popover so not all rules need apply
+    {id: "landmark-one-main",  enabled: false },
+    {id: "page-has-heading-one",  enabled: false }
+  ]
+});
+
+export default apparatus;
--- test/stories/chapters/turntable_part/subject.mjs
+++ test/stories/chapters/turntable_part/subject.mjs
@@ -4,46 +4,26 @@
 
 /* This files imports should be specified as part of the importmap in subject.page.pug */
 import { default as td } from 'testdouble';
-import { hold as zaHold, test as zaTest, report as zaReport, createTAPReporter } from 'zora';
 
 import { default as _tg } from './clones/_glods.clone.mjs';
 import { default as getTurntable } from 'turntable_part';
 
+import { default as helper } from './fixture.mjs';
 
-let
-  /** @type {Number} */
-  testCardIdx,
-  /** @type {string[]} */
-  zaTapLines = [];
+/** @type {Number} */
+let testCardIdx;
 const
   turntableID = '#turntable',
   /** @type {HTMLElement} */
-  $turntable = document.querySelector('#turntable'),
+  $turntable = document.querySelector(turntableID),
   /** @type {Part.Turntable} */
   turntableBehavior = getTurntable(turntableID),
-  logGlobals = () => { console.debug(_tg); },
   /** @type {CardIntri[]} */
-  testCards = [],
-  zaTapLogger = (/** @type {string} */ zaTapLogLine) => { zaTapLines.push(zaTapLogLine); },
-  zaTapSerializer = (/** @type {any} */ zaTapVal) => { return JSON.stringify(zaTapVal); },
-  zaTapReporter = createTAPReporter({ log: zaTapLogger, serialize: zaTapSerializer });
+  testCards = [];
 
 
-const getIdx = (cur = 0) => {
-  let result;
-
-  switch (true) {
-    case (cur >= _tg.c_Max): result = _tg.c_Max - 1; break;
-    case (cur < 0): result = 0; break;
-    default: result = cur;
-  }
-
-  return result;
-};
-
-
-const run_tests = () => {
-  zaTest('Should be ok', (z) => {
+const subjectTests = () => {
+  helper.chk('Should be ok', (z) => {
     let testResult, testExplntn;
     const mockObj = td.object(['hello']);
 
@@ -58,14 +38,7 @@ const run_tests = () => {
 };
 
 
-testCardIdx = getIdx();
-zaHold();
-window.axe.configure({
-  rules: [ // Just testing a popover so not all rules need apply
-    {id: "landmark-one-main",  enabled: false },
-    {id: "page-has-heading-one",  enabled: false }
-  ]
-});
+testCardIdx = helper.sanitize_card_id();
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('#card-sheet defs symbol:not(:has(rect))')?.forEach(($cSymbol, cIdx) => {
@@ -86,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ) {
     window.addEventListener('message', (_msgEvt) => {
       if (_msgEvt.data.type == 'print:globals') {
-        logGlobals();
+        console.debug(_tg);
       } else if (_msgEvt.data.type == 'subject:show') {
         $turntable.hidePopover();
         $turntable.showPopover();
@@ -97,15 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
         $turntable.hidePopover();
       } else if (_msgEvt.data.type == 'subject:test') {
         $turntable.hidePopover();
-        run_tests();
-        zaReport({ reporter: zaTapReporter }).then(() => {
-          window.__tap__ = zaTapLines.join('\n');
-          window.parent.postMessage({type: 'finished'});
-          zaTapLines = [];
-        }, (rejReason) => {
-          console.warn("issue with zora reporter");
-          console.debug(rejReason)
-        });
+        helper.run_tests(subjectTests);
       } else if (_msgEvt.data.type == 'subject:a11y') {
         window.axe.run().then((results) => {
           if (results.violations.length) {
