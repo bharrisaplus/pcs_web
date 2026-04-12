80be6567b3e93eb2f5a90eb604fc0ceb2cebbca1
Barry Harris <bharrisaplus@gmail.com>
2026-04-11 21:52:26 -0400

    Clean up

--- test/stories/chapters/turntable_part/fixture.mjs
+++ test/stories/chapters/turntable_part/fixture.mjs
@@ -2,34 +2,31 @@
 import { default as _tg } from './clones/_glods.clone.mjs';
 
 
-const get_bound_idx = (cur = 0) => {
-  let result;
-
-  switch (true) {
-    case (cur >= _tg.c_Max): result = _tg.c_Max - 1; break;
-    case (cur < 0): result = 0; break;
-    default: result = cur;
-  }
-
-  return result;
-};
-
-
 const getFixtures = () => {
   return Object.freeze({
-    sanitize_card_id: get_bound_idx,
+    sanitize_card_id: (cur = 0) => {
+      let result;
 
-    waaitt(ms = 2000) {
+      switch (true) {
+        case (cur >= _tg.c_Max): result = _tg.c_Max - 1; break;
+        case (cur < 0): result = 0; break;
+        default: result = cur;
+      }
+
+      return result;
+    },
+
+    waaitt: async (ms = 2000) => { // Shinynew but no Safari for now
       const fMS = ms >= 1000 ? ms : 1000;
 
       // oxlint-disable-next-line compat/compat
-      return window.scheduler.postTask(() => {}, { // Shinynew but no Safari for now
+      return window.scheduler.postTask(() => {}, {
         delay: fMS <= 30000 ? fMS : 30000,
         priority: 'user-blocking'
       });
     },
 
-    wait(msec = 2000) { // The classic
+    wait: async (msec = 2000) => { // The classic
       const fMSec = msec >= 1000 ? msec : 1000;
 
       return new Promise(function (resolve) {
