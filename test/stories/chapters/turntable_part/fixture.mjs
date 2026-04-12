6b830e82382b4a973569e600f396b675c0d7b8bd
Barry Harris <bharrisaplus@gmail.com>
2026-04-11 21:30:27 -0400

    Remove test runner code from fixture script

--- test/stories/chapters/turntable_part/fixture.mjs
+++ test/stories/chapters/turntable_part/fixture.mjs
@@ -1,14 +1,5 @@
-import { hold as zaHold, test as zaTest, report as zaReport, createTAPReporter } from 'zora';
 
 import { default as _tg } from './clones/_glods.clone.mjs';
-
-
-/** @type {string[]} */
-let zaTapLines = [];
-const
-  zaTapLogger = (/** @type {string} */ zaTapLogLine) => { zaTapLines.push(zaTapLogLine); },
-  zaTapSerializer = (/** @type {any} */ zaTapVal) => { return JSON.stringify(zaTapVal); },
-  zaTapReporter = createTAPReporter({ log: zaTapLogger, serialize: zaTapSerializer });
 
 
 const get_bound_idx = (cur = 0) => {
@@ -24,32 +15,8 @@ const get_bound_idx = (cur = 0) => {
 };
 
 
-/**
- * @param  {Function} runFnc
- */
-const test_routine = async (runFnc) => {
-  try {
-    await runFnc();
-  } catch (tstErr) {
-    console.warn("Issue during test routine");
-    console.error(tstErr);
-  }
-
-  zaReport({ reporter: zaTapReporter }).then(() => {
-    window.__tap__ = zaTapLines.join('\n');
-    window.parent.postMessage({type: 'finished'});
-    zaTapLines = [];
-  }, (rejReason) => {
-    console.warn("issue with zora reporter");
-    console.debug(rejReason)
-  });
-};
-
-
 const getFixtures = () => {
   return Object.freeze({
-    run_tests: test_routine,
-    zaTest: zaTest,
     sanitize_card_id: get_bound_idx,
 
     waaitt(ms = 2000) {
@@ -75,7 +42,6 @@ const getFixtures = () => {
 
 const apparatus = getFixtures();
 
-zaHold();
 window.axe.configure({
   rules: [ // Just testing a popover so not all rules need apply
     {id: "landmark-one-main",  enabled: false },
