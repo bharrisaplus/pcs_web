6dc887e00e6d9fb72b9557cae975a82d3abdec70
Barry Harris <bharrisaplus@gmail.com>
2026-04-11 21:30:55 -0400

    Add type for timestamp window global

--- test/stories/chapters/_.d.ts
+++ test/stories/chapters/_.d.ts
@@ -4,7 +4,8 @@ declare global {
   interface Window {
     printTestGlobals: () => void;
     __coverage__;
-    __tap__;
+    __tap__ :string;
+    __stampt__ :number;
     axe: {
       run: typeof axe.run,
       configure: typeof axe.configure
