65a81bc516d010d124ca5ae568da15d4e67cb697
Barry Harris <bharrisaplus@gmail.com>
2026-04-13 17:52:38 -0400

    Trim down clone

--- test/stories/chapters/ribbon_part/clones/_glods.clone.mjs
+++ test/stories/chapters/ribbon_part/clones/_glods.clone.mjs
@@ -3,11 +3,8 @@
  */
 
 
-/** @type {PCSEventTypes} */
+/** {@link PCSEventTypes} */
 const all_events = {
-  kick: 'kick',
-  needle: 'needle',
-  scratch: 'scratch',
   blend: "blend",
   chop: "chop",
   trace: "trace",
@@ -22,15 +19,11 @@ const _globals = Object.freeze({
   dyes: Object.freeze(['green-dye', 'red-dye', 'blue-dye', 'purple-dye']),
   pcs_cardTitle: 'Magnified view of card',
   pcs_cardDesc: 'A single card up close and personal',
-  pcs_cardRef: '#test-card',
+  pcs_cardRef: '#pcs-card',
   pcs_clippre: "Cards:\n====\n",
   c_Max: 14,
   c_TitlePrefix: 'Number',
-  c_DescPrefix: "Card in position",
-  c_SuiteList: Object.freeze(["Spade", "Diamond", "Club", "Heart"]),
-  c_NameList: Object.freeze([
-    'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'
-  ])
+  c_DescPrefix: "Card in position"
 });
 
 
--- test/stories/chapters/turntable_part/clones/_glods.clone.mjs
+++ test/stories/chapters/turntable_part/clones/_glods.clone.mjs
@@ -3,34 +3,21 @@
  */
 
 
-/** @type {PCSEventTypes} */
+/** {@link PCSEventTypes} */
 const all_events = {
-  kick: 'kick',
-  needle: 'needle',
-  scratch: 'scratch',
-  blend: "blend",
-  chop: "chop",
-  trace: "trace",
-  splash: "splash",
-  fresh: "fresh"
+  scratch: 'scratch'
 };
 
 /** {@link GlobalDeclarations} */
 const _globals = Object.freeze({
   appID: "test-shell",
   notices: Object.freeze(all_events),
-  dyes: Object.freeze(['green-dye', 'red-dye', 'blue-dye', 'purple-dye']),
   pcs_cardTitle: 'Magnified view of card',
   pcs_cardDesc: 'A single card up close and personal',
   pcs_cardRef: '#pcs-card',
-  pcs_clippre: "Cards:\n====\n",
   c_Max: 14,
   c_TitlePrefix: 'Number',
   c_DescPrefix: "Card in position",
-  c_SuiteList: Object.freeze(["Spade", "Diamond", "Club", "Heart"]),
-  c_NameList: Object.freeze([
-    'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'
-  ])
 });
 
 
