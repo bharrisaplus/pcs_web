2ff0f1810c1171660bd16379a3cab2664b38e67e
Barry Harris <bharrisaplus@gmail.com>
2026-04-15 01:20:11 -0400

    Add fixture module for ribbon tests

---
+++ test/stories/chapters/ribbon_part/fixture.mjs
@@ -0,0 +1,22 @@
+
+import { default as _tg } from './clones/_glods.clone.mjs';
+
+
+const getFixtures = () => {
+  return Object.freeze({
+    waaitt: async (ms = 2000) => {
+      const fMS = ms >= 1000 ? ms : 1000;
+
+      // oxlint-disable-next-line compat/compat
+      return window.scheduler.postTask(() => {}, {
+        delay: fMS <= 30000 ? fMS : 30000,
+        priority: 'user-blocking'
+      });
+    }
+  });
+};
+
+
+const apparatus = getFixtures();
+
+export default apparatus;
