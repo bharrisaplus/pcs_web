1f5df7064c54aa0b2d9923e61a16dd0742a60343
Barry Harris <bharrisaplus@gmail.com>
2026-04-15 00:26:26 -0400

    Clean up event listeners for ribbon part in apparatus when running tests

--- test/stories/chapters/ribbon_part/appa.mjs
+++ test/stories/chapters/ribbon_part/appa.mjs
@@ -9,62 +9,73 @@ import { default as getRibbon } from 'ribbon_part';
 import { default as ribbonTests } from './storey.mjs';
 
 
-let ribbonBehavior;
+let
+  /** @type {Part.Ribbon} */
+  ribbonBehavior,
+  listenController = new AbortController();
 const
   /** @type {HTMLElement} */
   $shell = document.querySelector(`#${_tg.appID}`),
   ribbonID = '#ribbon',
   /** @type {HTMLElement} */
-  $ribbon = document.querySelector(ribbonID);
+  $ribbon = document.querySelector(ribbonID),
+
+  setupListens = (/** @type {AbortSignal} */ listenSignal) => {
+    $shell.addEventListener(_tg.notices.blend, (/** @type {PCSEvent} */ _pcsevt) => {
+      if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.mingleBtn)) { return; }
+
+      console.debug("doing blend");
+    }, { signal: listenSignal });
+
+
+    $shell.addEventListener(_tg.notices.chop, (/** @type {PCSEvent} */ _pcsevt) => {
+      if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.copyBtn)) { return; }
+
+      console.debug("doing chop");
+    }, { signal: listenSignal });
+
+
+    $shell.addEventListener(_tg.notices.trace, (/** @type {PCSEvent} */ _pcsevt) => {
+      if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.downloadBtn)) { return; }
+
+      console.debug("doing trace");
+    }, { signal: listenSignal });
+
+
+    $shell.addEventListener(_tg.notices.splash, (/** @type {PCSEvent} */ _pcsevt) => {
+      if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.dyeInput)) { return; }
+
+      console.debug("doing splash");
+    }, { signal: listenSignal });
+
+
+    $shell.addEventListener(_tg.notices.fresh, (/** @type {PCSEvent} */ _pcsevt) => {
+      if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.clearBtn)) { return; }
+
+      console.debug("doing refresh");
+    }, { signal: listenSignal });
+  };
 
 
 ribbonBehavior = getRibbon(ribbonID);
 
 document.addEventListener('DOMContentLoaded', () => {
-  $shell.addEventListener(_tg.notices.blend, (/** @type {PCSEvent} */ _pcsevt) => {
-    if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.mingleBtn)) { return; }
-
-    console.debug("doing blend");
-  });
-
-
-  $shell.addEventListener(_tg.notices.chop, (/** @type {PCSEvent} */ _pcsevt) => {
-    if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.copyBtn)) { return; }
-
-    console.debug("doing chop");
-  });
-
-
-  $shell.addEventListener(_tg.notices.trace, (/** @type {PCSEvent} */ _pcsevt) => {
-    if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.downloadBtn)) { return; }
-
-    console.debug("doing trace");
-  });
-
-
-  $shell.addEventListener(_tg.notices.splash, (/** @type {PCSEvent} */ _pcsevt) => {
-    if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.dyeInput)) { return; }
-
-    console.debug("doing splash");
-  });
-
-
-  $shell.addEventListener(_tg.notices.fresh, (/** @type {PCSEvent} */ _pcsevt) => {
-    if (_pcsevt.detail.$dispatcher !== $ribbon.querySelector(ribbonBehavior.clearBtn)) { return; }
-
-    console.debug("doing refresh");
-  });
-
-
   if (
     window.frameElement &&
     window.parent.document.body.querySelectorAll(`${ribbonID}-panel`).length == 1
   ) {
+    setupListens(listenController.signal);
     window.addEventListener('message', (_msgEvt) => {
       if (_msgEvt.data.type == 'subject:test') {
-        ribbonTests.go().then(() => {}, (rejRsn) => {
+        listenController.abort();
+        ribbonTests.go().then(() => {
+          listenController = new AbortController();
+          setupListens(listenController.signal);
+        }, (rejRsn) => {
           console.warn("Issue with running test(s)");
           console.error(rejRsn);
+          listenController = new AbortController();
+          setupListens(listenController.signal);
         });
       } else if (_msgEvt.data.type == 'subject:a11y') {
         window.axe.run().then((results) => {
