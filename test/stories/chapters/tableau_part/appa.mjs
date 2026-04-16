04a72b95acbe300d404d0731c153dd8df1940b1d
Barry Harris <bharrisaplus@gmail.com>
2026-04-16 01:19:13 -0400

    Load turntable part in storey

--- test/stories/chapters/tableau_part/appa.mjs
+++ test/stories/chapters/tableau_part/appa.mjs
@@ -1,15 +1,37 @@
+/**
+ * @import {PCSEvent} from 'pcs:types'
+ */
 
 /* This files imports should be specified as part of the importmap in subject.page.pug */
 import { default as _tg } from './clones/_glods.clone.mjs';
 
+import { default as getTableau } from 'tableau_part';
+
+
+let listenController = new AbortController();
 const
   tableauID = '#tableau',
   /** @type {HTMLElement} */
   $shell = document.querySelector(`#${_tg.appID}`),
   /** @type {HTMLElement} */
-  $tableau = $shell.querySelector(tableauID);
+  $tableau = $shell.querySelector(tableauID),
+  tableauBehavior = getTableau(tableauID);
+
+
+const handleTableauNeedle = (/** @type {PCSEvent} */ _pcsevt) => {
+  const msgString = `
+    id: ${_pcsevt.detail.$dispatcher.getAttribute('id')}
+    data-oid: ${_pcsevt.detail.$dispatcher.getAttribute('data-oid')}
+    aria-description: ${_pcsevt.detail.$dispatcher.getAttribute('aria-description')}
+  `;
+
+  console.log("From tableau\n" + msgString);
+};
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  $shell.addEventListener(_tg.notices.needle, handleTableauNeedle, { signal: listenController.signal });
+
   $tableau.setAttribute('style', '');
   $tableau.classList.remove('hide-before-load');
 
@@ -20,6 +42,11 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('message', (_msgEvt) => {
       if (_msgEvt.data.type == 'subject:test') {
         console.debug("run tests");
+        listenController.abort();
+
+        listenController = new AbortController();
+
+        $shell.addEventListener(_tg.notices.needle, handleTableauNeedle, { signal: listenController.signal });
       } else if (_msgEvt.data.type == 'subject:a11y') {
         window.axe.run().then((results) => {
           if (results.violations.length) {
