ce14a5024c1062ed256cecf8d21d8e725922553a
Barry Harris <bharrisaplus@gmail.com>
2026-04-16 17:51:20 -0400

    Remove empty line

--- test/stories/chapters/tableau_part/appa.mjs
+++ test/stories/chapters/tableau_part/appa.mjs
@@ -5,7 +5,6 @@
 /* This files imports should be specified as part of the importmap in subject.page.pug */
 import { default as _tg } from './clones/_glods.clone.mjs';
 import { default as tableauTests } from './storey.mjs';
-
 import { default as getTableau } from 'tableau_part';
 
 
