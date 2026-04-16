42bb03a7724fd0c3673e10715978f61d13583ffc
Barry Harris <bharrisaplus@gmail.com>
2026-04-16 17:59:01 -0400

    Use tanto module instead of fixture.mjs in ribbon storey

--- test/stories/chapters/ribbon_part/storey.mjs
+++ test/stories/chapters/ribbon_part/storey.mjs
@@ -8,7 +8,7 @@ import { hold as zaHold, createHarness, createTAPReporter } from 'zora';
 import { default as td } from 'testdouble';
 
 import { default as _tg } from './clones/_glods.clone.mjs';
-import { default as fx } from './fixture.mjs';
+import { default as fx } from 'storey:fixtures';
 
 
 const
