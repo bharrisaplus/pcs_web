0930aa8a40870632f39cccd6b499ea5f01050ae6
Barry Harris <bharrisaplus@gmail.com>
2026-04-16 23:29:29 -0400

    Tabs to spaces

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,24 +1,24 @@
 export default {
-	extends: [
-		'stylelint-stylus/standard'
-	],
-	"ignoreFiles": ['**/README.md'],
-	plugins: [
-		'stylelint-stylus',
-		'stylelint-no-unsupported-browser-features'
-	],
-	rules: {
-		"stylus/declaration-colon": 'always',
-		"stylus/indentation": 4,
-		"stylus/selector-list-comma": 'always',
-		"stylus/color-hex-case": null,
-		"stylus/media-feature-colon": "always",
-		"stylus/single-line-comment": null,
-		"plugin/no-unsupported-browser-features": [ true,
-			{
-				"ignore": ["css-clip-path"],
-				"severity": 'warning'
-			}
-		]
-	}
+  extends: [
+    'stylelint-stylus/standard'
+  ],
+  "ignoreFiles": ['**/README.md'],
+  plugins: [
+    'stylelint-stylus',
+    'stylelint-no-unsupported-browser-features'
+  ],
+  rules: {
+    "stylus/declaration-colon": 'always',
+    "stylus/indentation": 4,
+    "stylus/selector-list-comma": 'always',
+    "stylus/color-hex-case": null,
+    "stylus/media-feature-colon": "always",
+    "stylus/single-line-comment": null,
+    "plugin/no-unsupported-browser-features": [ true,
+      {
+        "ignore": ["css-clip-path"],
+        "severity": 'warning'
+      }
+    ]
+  }
 };
