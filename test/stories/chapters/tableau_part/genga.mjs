6a1e29893cac871438e172ac1add06c0ce310ed8
Barry Harris <bharrisaplus@gmail.com>
2026-04-17 16:53:27 -0400

    Add test to tableau storey

--- test/stories/chapters/tableau_part/genga.mjs
+++ test/stories/chapters/tableau_part/genga.mjs
@@ -1,5 +1,5 @@
 /**
- * @import { CardIntri, CSSelector, Part } from 'pcs:types'
+ * @import { PCSEvent, CardIntri, CSSelector, Part } from 'pcs:types'
  * @import { ITestFunction } from 'zora';
  */
 
@@ -71,6 +71,7 @@ const tableau_part_tests = async (testInfos, zaTest) => {
     console.error(t1E);
   }
 
+
   try {
     await zaTest('Should handle click', async (z) => {
       let
@@ -119,6 +120,77 @@ const tableau_part_tests = async (testInfos, zaTest) => {
   } catch (t2E) {
     console.error(t2E);
   }
+
+
+  await zaTest("Should handle update", async (z) => {
+    let
+      /** @type {HTMLElement[]} */
+      $items,
+      /** @type {Array<string[]>} */
+      swearUIRslts = [[], []],
+      swearBhvRslts = {
+        /** @type {Array<number[]>} */
+        orderChks: [],
+        /** @type {string[]} */
+        eventChks: [],
+        /** @type {string[]} */
+        dispatcherChks: []
+      };
+    const
+      swearPicks = [Math.floor(Math.random() * _tg.c_Max), Math.floor(Math.random() * _tg.c_Max)],
+      swearOGOIDs = testInfos.map((_card) => { return _card.oglo }),
+      swearOG$Order = testInfos.map((_card) => { return `card-${_card.symbolRef.split("#")[1]}` }),
+      swearMixCards = testInfos.filter(
+        (_, _idx) => { return _idx % 2 == 0 }
+      ).concat(testInfos.filter(
+        (_, _idx) => {return _idx % 2 == 1 }
+      )),
+
+      swearMixOIDs = swearMixCards.map((_card) => { return _card.oglo }),
+      swearMix$Order = swearMixCards.map((_card) => { return `card-${_card.symbolRef.split("#")[1]}` }),
+
+      abCntrllr = new AbortController(),
+      impMeta = await getImport(),
+      /** @type {HTMLBodyElement} */
+      $testshell = document.querySelector(`#${_tg.appID}`);
+
+
+    $testshell.addEventListener(_tg.notices.needle, (/** @type {PCSEvent} */ _pcsevt) => {
+      swearBhvRslts.eventChks.push(_tg.notices.needle);
+      swearBhvRslts.dispatcherChks.push(_pcsevt.detail?.$dispatcher?.dataset.oid);
+    }, { signal: abCntrllr.signal });
+
+    $items = Array.from(document.querySelectorAll(`${tableauID} ${tableauItemSelector}`));
+
+    swearBhvRslts.orderChks.push(impMeta.freshModule.currentOrder);
+    $items.forEach(($item) => { swearUIRslts[0].push($item.getAttribute('id')); });
+
+    $items[swearPicks[0]]?.click();
+    await fx.waaitt(100);
+    impMeta.freshModule.updateOrder(swearMixCards);
+    await fx.waaitt(100);
+    $items[swearPicks[1]]?.click();
+
+    $items = Array.from(document.querySelectorAll(`${tableauID} ${tableauItemSelector}`));
+
+    swearBhvRslts.orderChks.push(impMeta.freshModule.currentOrder);
+    $items.forEach(($item) => { swearUIRslts[1].push($item.getAttribute('id')); });
+
+    td.reset();
+    impMeta.freshModule = null;
+    abCntrllr.abort();
+
+
+    z.same(swearBhvRslts.orderChks[0].toString(), swearOGOIDs.toString(), "item order before is correct");
+    z.same(swearUIRslts[0].toString(), swearOG$Order.toString(), "$element order before is correct");
+    z.same(swearBhvRslts.eventChks.toString(), "needle,needle", "events fired as expected");
+    z.same(
+      swearBhvRslts.dispatcherChks.toString(), `${swearOGOIDs[swearPicks[0]]},${swearMixOIDs[swearPicks[1]]}`,
+      "event fired expected $elements"
+    );
+    z.same(swearBhvRslts.orderChks[1].toString(), swearMixOIDs.toString(), "item order after is correct");
+    z.same(swearUIRslts[1].toString(), swearMix$Order.toString(), "$element order after is correct");
+  });
 };
 
 
