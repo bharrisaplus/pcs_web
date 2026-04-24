d03fdcdbf355f60573569541a72560b49f9d7922
Barry Harris <bharrisaplus@gmail.com>
2026-04-23 23:50:55 -0400

    Adjust spacing in specs from 4 -> 2

--- source/behavior/hands/scribe.hand.mjs
+++ source/behavior/hands/scribe.hand.mjs
@@ -9,144 +9,144 @@
  * @return {Hand.Scribe} a logger - {@link Hand.Scribe}
  */
 const makeScribeHand = () => {
-	/** @type {NotificationToast[]} */
-	let toasts = [];
-	const
-		console_free = (
-		  window.location?.href?.startsWith('http://localhost:') ||
-		  window.location?.href?.startsWith('https://localhost:') ||
-		  window.location?.href?.startsWith('file:') // || whatever you like
-		),
+  /** @type {NotificationToast[]} */
+  let toasts = [];
+  const
+    console_free = (
+      window.location?.href?.startsWith('http://localhost:') ||
+      window.location?.href?.startsWith('https://localhost:') ||
+      window.location?.href?.startsWith('file:') // || whatever you like
+    ),
 
-		/** @type {HTMLElement} */
-		$toaster = document.querySelector('#notifications'),
-		/** @type {HTMLTemplateElement} */
-		$toastRef = document.querySelector('template#toast');
-
-
-	/**
-	 * @param  {string} devMsg
-	 * @param  {Object} [devThingy]
-	 * @see Hand.Scribe#devlog
-	 */
-	const log_dev = (devMsg, devThingy) => {
-		if (console_free) {
-			if (devMsg) { console.debug(devMsg); }
-			if (devThingy) { console.debug(devThingy); }
-		}
-	};
+    /** @type {HTMLElement} */
+    $toaster = document.querySelector('#notifications'),
+    /** @type {HTMLTemplateElement} */
+    $toastRef = document.querySelector('template#toast');
 
 
-	/**
-	 * @param  {string} issueMsg
-	 * @param  {Object} [issueThingy]
-	 * @param  {unknown|Error|DOMException} [issueErr]
-	 * @param  {boolean} [blocking]
-	 * @see Hand.Scribe#issuelog
-	 */
-	const log_issue = (issueMsg, issueThingy, issueErr, blocking = true) => {
-		if (console_free) {
-			if (blocking) {
-				if (issueMsg) { console.error(issueMsg); }
-				if (issueThingy) { console.debug(issueThingy); }
-				if (issueErr) { console.error(issueErr); }
-			} else {
-				if (issueMsg) { console.warn(issueMsg); }
-				if (issueThingy) { console.debug(issueThingy); }
-			}
-		}
-	};
+  /**
+   * @param  {string} devMsg
+   * @param  {Object} [devThingy]
+   * @see Hand.Scribe#devlog
+   */
+  const log_dev = (devMsg, devThingy) => {
+    if (console_free) {
+      if (devMsg) { console.debug(devMsg); }
+      if (devThingy) { console.debug(devThingy); }
+    }
+  };
 
 
-	/** @param  {string} maybeMsg */
-	const _make_toast = (maybeMsg) => {
-		/** @type {NotificationToast} */
-		let maybeNoTo;
-		const
-			aToastAbort = new AbortController(),
-			/** @type {DocumentFragment} */
-			$toastContainer = document.importNode($toastRef.content, true),
-			/** @type {HTMLElement} */
-			$aToast = $toastContainer.querySelector('.toast'),
-			/** @type {HTMLSpanElement} */
-			$aToastMsg = $aToast.querySelector('span.toast-msg'),
-			/** @type {HTMLButtonElement} */
-			$aToastClose = $aToast.querySelector('button.toast-close');
+  /**
+   * @param  {string} issueMsg
+   * @param  {Object} [issueThingy]
+   * @param  {unknown|Error|DOMException} [issueErr]
+   * @param  {boolean} [blocking]
+   * @see Hand.Scribe#issuelog
+   */
+  const log_issue = (issueMsg, issueThingy, issueErr, blocking = true) => {
+    if (console_free) {
+      if (blocking) {
+        if (issueMsg) { console.error(issueMsg); }
+        if (issueThingy) { console.debug(issueThingy); }
+        if (issueErr) { console.error(issueErr); }
+      } else {
+        if (issueMsg) { console.warn(issueMsg); }
+        if (issueThingy) { console.debug(issueThingy); }
+      }
+    }
+  };
 
 
-		if (!maybeMsg || maybeMsg.trim().length < 10) { return; }
-
-		if (!$toaster || !$toastRef || !$aToast || !$aToastMsg || !$aToastClose) { return; }
-
-		if (toasts.some((_toast) => _toast.$elm.querySelector('span.toast-msg')?.textContent === maybeMsg)) {
-			return;
-		}
-
-
-		$aToastMsg.textContent = maybeMsg;
-		$aToastClose.addEventListener('click', (_clickEvt) => {
-			let cutIdx, cutToast;
-
-			if (
-				!(_clickEvt.target instanceof window.HTMLButtonElement) ||
-				_clickEvt.target.parentElement !== $aToast
-			) { return; }
-
-			cutIdx = toasts.map((_toastItm) => _toastItm.$elm ).indexOf($aToast);
-
-			if (toasts.length === 1) {
-				$toaster.hidePopover();
-			}
-
-			if (cutIdx === -1) {
-				$aToast.remove();
-				return;
-			}
-
-			cutToast = toasts[cutIdx];
-			toasts = toasts.toSpliced(cutIdx, 1);
-
-			cutToast.cancel.abort();
-			cutToast.$elm.remove();
-		}, {signal: aToastAbort.signal});
-
-		$toaster.appendChild($aToast);
-		$toaster.showPopover();
-
-		if (toasts.length >= 3) {
-			const _gcToast = toasts.shift();
-
-			_gcToast.cancel.abort();
-			_gcToast.$elm.remove();
-		}
-
-		maybeNoTo = {$elm: $aToast, cancel: aToastAbort}
-
-		toasts.push(maybeNoTo);
-	};
-
-	/**
-	 * @param  {string} notificationMsg
-	 * @see Hand.Scribe#notilog
-	 */
-	const log_notification = (notificationMsg) => {
-		if (console_free) {
-			if (notificationMsg) { console.info(notificationMsg); }
-		}
-
-		if ($toastRef && $toaster?.matches(`[popover="manual"]`)) {
-			_make_toast(notificationMsg);
-		}
-	};
+  /** @param  {string} maybeMsg */
+  const _make_toast = (maybeMsg) => {
+    /** @type {NotificationToast} */
+    let maybeNoTo;
+    const
+      aToastAbort = new AbortController(),
+      /** @type {DocumentFragment} */
+      $toastContainer = document.importNode($toastRef.content, true),
+      /** @type {HTMLElement} */
+      $aToast = $toastContainer.querySelector('.toast'),
+      /** @type {HTMLSpanElement} */
+      $aToastMsg = $aToast.querySelector('span.toast-msg'),
+      /** @type {HTMLButtonElement} */
+      $aToastClose = $aToast.querySelector('button.toast-close');
 
 
-	window['devToast'] = console_free ? log_notification : undefined;
+    if (!maybeMsg || maybeMsg.trim().length < 10) { return; }
 
-	return Object.freeze({
-		devlog: log_dev,
-		issuelog: log_issue,
-		notilog: log_notification
-	});
+    if (!$toaster || !$toastRef || !$aToast || !$aToastMsg || !$aToastClose) { return; }
+
+    if (toasts.some((_toast) => _toast.$elm.querySelector('span.toast-msg')?.textContent === maybeMsg)) {
+      return;
+    }
+
+
+    $aToastMsg.textContent = maybeMsg;
+    $aToastClose.addEventListener('click', (_clickEvt) => {
+      let cutIdx, cutToast;
+
+      if (
+        !(_clickEvt.target instanceof window.HTMLButtonElement) ||
+        _clickEvt.target.parentElement !== $aToast
+      ) { return; }
+
+      cutIdx = toasts.map((_toastItm) => _toastItm.$elm ).indexOf($aToast);
+
+      if (toasts.length === 1) {
+        $toaster.hidePopover();
+      }
+
+      if (cutIdx === -1) {
+        $aToast.remove();
+        return;
+      }
+
+      cutToast = toasts[cutIdx];
+      toasts = toasts.toSpliced(cutIdx, 1);
+
+      cutToast.cancel.abort();
+      cutToast.$elm.remove();
+    }, {signal: aToastAbort.signal});
+
+    $toaster.appendChild($aToast);
+    $toaster.showPopover();
+
+    if (toasts.length >= 3) {
+      const _gcToast = toasts.shift();
+
+      _gcToast.cancel.abort();
+      _gcToast.$elm.remove();
+    }
+
+    maybeNoTo = {$elm: $aToast, cancel: aToastAbort}
+
+    toasts.push(maybeNoTo);
+  };
+
+  /**
+   * @param  {string} notificationMsg
+   * @see Hand.Scribe#notilog
+   */
+  const log_notification = (notificationMsg) => {
+    if (console_free) {
+      if (notificationMsg) { console.info(notificationMsg); }
+    }
+
+    if ($toastRef && $toaster?.matches(`[popover="manual"]`)) {
+      _make_toast(notificationMsg);
+    }
+  };
+
+
+  window['devToast'] = console_free ? log_notification : undefined;
+
+  return Object.freeze({
+    devlog: log_dev,
+    issuelog: log_issue,
+    notilog: log_notification
+  });
 };
 
 
--- source/behavior/shuttles/host.shuttle.mjs
+++ source/behavior/shuttles/host.shuttle.mjs
@@ -10,69 +10,69 @@ import { default as appLogger } from '../hands/scribe.hand.mjs';
 /** @return {Readonly<Shuttle.Host>} {@link Shuttle.Host} */
 const makeHostShuttle = () => {
 
-	/**
-	 * @param  {string} resourceLink {@link URL}
-	 *
-	 * @return {Promise<Blob>}
-	 */
-	const blob_from_url = async (resourceLink) => {
-		let
-			/** @type {Response} */
-			blobResponse,
-			/** @type {Blob} */
-			blobResult;
+  /**
+   * @param  {string} resourceLink {@link URL}
+   *
+   * @return {Promise<Blob>}
+   */
+  const blob_from_url = async (resourceLink) => {
+    let
+      /** @type {Response} */
+      blobResponse,
+      /** @type {Blob} */
+      blobResult;
 
-		try {
-			blobResponse = await fetch(resourceLink);
+    try {
+      blobResponse = await fetch(resourceLink);
 
-			if (!blobResponse.ok) {
-				if (blobResponse.status > 399) {
-					appLogger.issuelog("Network issue with file grab", {resourceLink, blobResponse}, null);
-				}
+      if (!blobResponse.ok) {
+        if (blobResponse.status > 399) {
+          appLogger.issuelog("Network issue with file grab", {resourceLink, blobResponse}, null);
+        }
 
-				blobResponse = null;
-			}
-		} catch (fetchErr) {
-			let fetchErrMsg = "Some problem grabbing file";
+        blobResponse = null;
+      }
+    } catch (fetchErr) {
+      let fetchErrMsg = "Some problem grabbing file";
 
-			if (fetchErr instanceof DOMException && fetchErr.name === "AbortError") {
-				fetchErrMsg = "Aborted file grab";
-			} else if (fetchErr instanceof DOMException && fetchErr.name === "NotAllowedError") {
-				fetchErrMsg = "Permission issue with file grab";
-			} else if (fetchErr instanceof TypeError) {
-				fetchErrMsg = "Type mismatch with file grab";
-			}
+      if (fetchErr instanceof DOMException && fetchErr.name === "AbortError") {
+        fetchErrMsg = "Aborted file grab";
+      } else if (fetchErr instanceof DOMException && fetchErr.name === "NotAllowedError") {
+        fetchErrMsg = "Permission issue with file grab";
+      } else if (fetchErr instanceof TypeError) {
+        fetchErrMsg = "Type mismatch with file grab";
+      }
 
-			appLogger.issuelog(fetchErrMsg, {resourceLink, blobResponse, fetchErr}, null);
-			blobResponse = null;
-		}
+      appLogger.issuelog(fetchErrMsg, {resourceLink, blobResponse, fetchErr}, null);
+      blobResponse = null;
+    }
 
-		if (blobResponse) {
-			try {
-				blobResult = await blobResponse.blob();
-			} catch (readErr) {
-				let readErrMsg = "Some problem reading file"
+    if (blobResponse) {
+      try {
+        blobResult = await blobResponse.blob();
+      } catch (readErr) {
+        let readErrMsg = "Some problem reading file"
 
-				if (readErr instanceof DOMException && readErr.name === "AbortError") {
-					readErrMsg = "Aborted file read";
-				} else if (readErr instanceof TypeError) {
-					readErrMsg = "Could not read file";
-				}
+        if (readErr instanceof DOMException && readErr.name === "AbortError") {
+          readErrMsg = "Aborted file read";
+        } else if (readErr instanceof TypeError) {
+          readErrMsg = "Could not read file";
+        }
 
-				appLogger.issuelog(readErrMsg, {resourceLink, blobResponse, readErr}, null);
-				blobResult = null;
-			}
-		}
+        appLogger.issuelog(readErrMsg, {resourceLink, blobResponse, readErr}, null);
+        blobResult = null;
+      }
+    }
 
-		return blobResult;
-	}
+    return blobResult;
+  }
 
-	/** @type {Shuttle.Host} */
-	const HostShuttle = Object.freeze({
-		grabFile: blob_from_url
-	});
+  /** @type {Shuttle.Host} */
+  const HostShuttle = Object.freeze({
+    grabFile: blob_from_url
+  });
 
-	return HostShuttle;
+  return HostShuttle;
 };
 
 export default makeHostShuttle;
