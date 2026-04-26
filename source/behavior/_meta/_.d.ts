ce62c2825bf1a0c11e8934eb2416b11b564d42bd
Barry Harris <bharrisaplus@gmail.com>
2026-04-26 13:38:37 -0400

    Remove ChanceJS from build files and remove script tag from html

--- source/behavior/_meta/_.d.ts
+++ source/behavior/_meta/_.d.ts
@@ -1,12 +1,5 @@
 // oxlint-disable @stylistic/max-len
 
-/**
- * Type definitions for using ChanceJS
- *     adapted from -> npm:@types/chance
- */
-interface chanceJS {
-  pickset<T>(arr :T[], count ?:number) :T[];
-};
 
 declare global {
   // Tweaks for lib.dom.d.ts
@@ -32,9 +25,6 @@ declare global {
     localhost_url: string;
     es_main: string;
   };
-
-  // ChanceJS browser global
-  const chance :chanceJS;
 }
 
 export {};
