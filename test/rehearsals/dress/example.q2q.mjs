aad0be8c9763040d5c5ce4d651feb7bd2ebe0321
Barry Harris <bharrisaplus@gmail.com>
2026-05-04 21:07:45 -0400

    Adjust test to check page

--- test/rehearsals/dress/example.q2q.mjs
+++ test/rehearsals/dress/example.q2q.mjs
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Example', () => {
-  test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+import { default as testShared } from '../../compass.mjs';
 
-    await expect(page).toHaveTitle(/Playwright/);
+test.describe('Example', () => {
+  test('has title', async ({ page: pwPage }) => {
+    await pwPage.goto(`http://localhost:${testShared.SERVER_PORT}/`);
+
+    await expect(pwPage).toHaveTitle("pcs:web");
   });
 });
